cf8fc141035cfb0284bec18182a94efe7bceb38d
Ashot Vardanian <ashvardanian@localhost>
2022-06-13 22:46:29 +0400

    Implemented sub-collections

--- python/test.py
+++ python/test.py
@@ -2,11 +2,38 @@ import ukv
 
 
 if __name__ == '__main__':
-    col = ukv.UKV()
-    col[1] = 'a'.encode()
-    col[2] = 'bb'.encode()
-    col[3] = 'c'.encode()
+    db = ukv.DataBase()
+    with db as col:
+        print('opened')
 
-    print(col[1])
-    print(col[2])
-    print(col[3])
+        col[1] = 'a'.encode()
+        col[2] = 'bb'.encode()
+        col[3] = 'c'.encode()
+        print('filled')
+
+        print(col[1])
+        print(col[2])
+        print(col[3])
+        print('exported')
+
+        col.set('doubled', 1, 'aa'.encode())
+        col.set('doubled', 2, 'bbbb'.encode())
+        col.set('doubled', 3, 'cc'.encode())
+        print(col.get('doubled', 1))
+        print(col.get('doubled', 2))
+        print(col.get('doubled', 3))
+
+    # with ukv.DataBase() as db:
+
+    #     with ukv.Transaction(db, 10) as txn:
+    #         col = txn['main']
+
+    #         col[1] = 'a'.encode()
+    #         col[2] = 'bb'.encode()
+    #         col[3] = 'c'.encode()
+    #         print('filled')
+
+    #         print(col[1])
+    #         print(col[2])
+    #         print(col[3])
+    #         print('exported')
