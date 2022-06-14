fecb11890170f8c16afa10ff121fd29b0094a423
Ashot Vardanian <ashvardanian@localhost>
2022-06-14 17:53:02 +0400

    Using read/write generic ops to replace many more In particular, those can conver exists/get and insert/update/upsert/delete.

--- include/ukv.h
+++ include/ukv.h
@@ -5,6 +5,10 @@
  * @brief C bindings for Unums Key-Value store, that provide:
  * > ABI stability for the essential CRUD operations,
  * > Interoperability with higher-level languages.
+ * Assumptions and limitations:
+ * > Keys are preset to 8-byte unsigned integers.
+ * > Zero-length values are not allowed.
+ * > Iterators often can't be fully consistent, to allow concurrency.
  *
  * @todo Iterators over transactions/snapshots.
  * @todo Manual state control over compactions.
@@ -29,6 +33,17 @@
  *    so the DB shouldn't die/close before those objects are freed.
  *    This also allows us to reduce the number of function arguments for
  *    interface functions.
+ *
+ * @section Choosing between more functions vs more argument per function
+ * We try to preserve balance in the number of function calls exposed in
+ * this C API/ABI layer and the complexity of each call. As a result,
+ * the @b set methods can be used to:
+ * > insert
+ * > update
+ * > detele
+ * and the @b get methods can be used to:
+ * > check object existance
+ * > retrieve an object
  *
  * @section Reference Designs
  * This interface is design as a generalization over most CRUD APIs
@@ -94,7 +109,7 @@ void ukv_open(
  * @brief The primary "setter" interface that inserts into "HEAD" state.
  * Passing NULLs into @p `values` is identical to deleting entries.
  * If a fail had occured, @p `error` will be set to non-NULL.
- * @see `ukv_txn_put` for transactional consistent inserts.
+ * @see `ukv_txn_write` for transactional consistent inserts.
  *
  * @param db                Already open database instance, @see `ukv_open`.
  * @param keys              Array of keys in one or more collections.
@@ -111,7 +126,7 @@ void ukv_open(
  * @param values_lengths    Array of lengths of buffers, storing the @p `values`.
  * @param error             The error to be handled.
  */
-void ukv_put(
+void ukv_write(
     // Inputs:
     ukv_t const db,
     ukv_key_t const* keys,
@@ -126,50 +141,10 @@ void ukv_put(
     ukv_error_t* error);
 
 /**
- * @brief Checks if certain keys are present in passed collections.
- *
- * @param db                Already open database instance, @see `ukv_open`.
- * @param keys              Array of keys in one or more collections.
- * @param keys_count        Number of elements in @p `keys`.
- * @param columns           Array of columns with 0, 1 or `keys_count`
- *                          elements describing the locations of keys.
- *                          If NULL is passed, the default collection
- *                          is assumed.
- * @param columns_count     Number of elements in @p `columns`.
- * @param options           Write options.
- * @param arena             Points to a memory region that we use during
- *                          this request. If it's too small (@p `arena_length`),
- *                          we `realloc` a new buffer. You can't pass a memory
- *                          allocated by a third-party allocator.
- *                          During the first request you pass a `NULL`,
- *                          we allocate that buffer, put found values in it and
- *                          return you a pointer. You can later reuse it for future
- *                          requests, or `free` it via `ukv_get_free`.
- * @param arena_length      Current size of @p `arena`.
- * @param values            Array of @p `keys_count` output boolean indicators.
- * @param error             The error message to be handled by callee.
- */
-void ukv_contains(
-    // Inputs:
-    ukv_t const db,
-    ukv_key_t const* keys,
-    size_t const keys_count,
-    ukv_column_t const* columns,
-    size_t const columns_count,
-
-    // In-outs:
-    void** arena,
-    size_t* arena_length,
-
-    // Outputs:
-    bool* values,
-    ukv_error_t* error);
-
-/**
  * @brief The primary "getter" interface, that reads from "HEAD" state.
  * If a fail had occured, @p `error` will be set to non-NULL.
  * If a key wasn't found in target collection, the value is empty.
- * @see `ukv_txn_put` for transactional consistent lookups.
+ * @see `ukv_txn_write` for transactional consistent lookups.
  *
  * @param db                Already open database instance, @see `ukv_open`.
  * @param keys              Array of keys in one or more collections.
@@ -187,13 +162,15 @@ void ukv_contains(
  *                          During the first request you pass a `NULL`,
  *                          we allocate that buffer, put found values in it and
  *                          return you a pointer. You can later reuse it for future
- *                          requests, or `free` it via `ukv_get_free`.
+ *                          requests, or `free` it via `ukv_read_free`.
  * @param arena_length      Current size of @p `arena`.
  * @param values            Array of pointers to the first byte of each value.
- * @param values_lengths    Array of lengths of buffers, storing the @p `values`.
+ *                          If `NULL`, only the `value_lengths` will be pulled.
+ * @param values_lengths    Lengths of the values. Zero means value is missing.
+ *                          Can't be `NULL`.
  * @param error             The error message to be handled by callee.
  */
-void ukv_get(
+void ukv_read(
     // Inputs:
     ukv_t const db,
     ukv_key_t const* keys,
@@ -216,9 +193,9 @@ void ukv_get(
 /*********************************************************/
 
 /**
- * @brief Upserts a new column into DB.
- * As of now, only the default column is available
- * and @p `column_name` is ignored across the interaface.
+ * @brief Upserts a new named column into DB.
+ * This function may never be called, as the default
+ * unnamed collection always exists.
  */
 void ukv_column_upsert(
     // Inputs:
@@ -228,6 +205,10 @@ void ukv_column_upsert(
     ukv_column_t* column,
     ukv_error_t* error);
 
+/**
+ * @brief Removes column and all of its conntents from DB.
+ * The default unnamed collection can't be removed.
+ */
 void ukv_column_remove(
     // Inputs:
     ukv_t const db,
@@ -267,7 +248,7 @@ void ukv_txn_begin(
     ukv_txn_t* txn,
     ukv_error_t* error);
 
-void ukv_txn_put(
+void ukv_txn_write(
     // Inputs:
     ukv_txn_t const txn,
     ukv_key_t const* keys,
@@ -280,7 +261,7 @@ void ukv_txn_put(
     // Outputs:
     ukv_error_t* error);
 
-void ukv_txn_get(
+void ukv_txn_read(
     // Inputs:
     ukv_t const db,
     ukv_key_t const* keys,
@@ -320,23 +301,23 @@ void ukv_iter_seek(ukv_iter_t const iter, ukv_key_t key, ukv_error_t* error);
 
 void ukv_iter_advance(ukv_iter_t const iter, size_t const count_to_skip, ukv_error_t* error);
 
-void ukv_iter_get_key(ukv_iter_t const iter, ukv_key_t*, ukv_error_t* error);
+void ukv_iter_read_key(ukv_iter_t const iter, ukv_key_t*, ukv_error_t* error);
 
-void ukv_iter_get_value_size(ukv_iter_t const iter, size_t* length, size_t* arena_length, ukv_error_t* error);
+void ukv_iter_read_value_size(ukv_iter_t const iter, size_t* length, size_t* arena_length, ukv_error_t* error);
 
 /**
  * @brief Fetches currently targeted value from disk.
  * ! The entry received from yere should NOT
- * ! be deallocated via `ukv_get_free`.
+ * ! be deallocated via `ukv_read_free`.
  */
-void ukv_iter_get_value(ukv_iter_t const iter,
-                        // In-outs:
-                        void** arena,
-                        size_t* arena_length,
-                        // Outputs:
-                        ukv_val_ptr_t* values,
-                        ukv_val_len_t* values_lengths,
-                        ukv_error_t* error);
+void ukv_iter_read_value(ukv_iter_t const iter,
+                         // In-outs:
+                         void** arena,
+                         size_t* arena_length,
+                         // Outputs:
+                         ukv_val_ptr_t* value,
+                         ukv_val_len_t* value_lengths,
+                         ukv_error_t* error);
 
 /*********************************************************/
 /*****************	  Memory Management   ****************/
@@ -344,11 +325,11 @@ void ukv_iter_get_value(ukv_iter_t const iter,
 
 /**
  * @brief A function to be used in conjunction with:
- * > `ukv_get`
- * > `ukv_txn_get`
+ * > `ukv_read`
+ * > `ukv_txn_read`
  * to deallocate and return memory to UnumDB and OS.
  */
-void ukv_get_free(ukv_t const db, void*, size_t);
+void ukv_read_free(ukv_t const db, void*, size_t);
 
 void ukv_txn_free(ukv_t const db, ukv_txn_t const txn);
 
