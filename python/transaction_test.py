16d84bd670fe35ae7e3e9949a2748acaf582d69c
davit <davit@localhost>
2022-06-22 16:17:21 +0000

    Minor

--- python/transaction_test.py
+++ python/transaction_test.py
@@ -89,23 +89,23 @@ def test_set_by_2_intersected_transactions():
         assert db[index] == 'b'.encode()
 
 
-# def test_remove_by_2_intersected_transactions():
-#     keys_count = 100
+def test_remove_by_2_intersected_transactions():
+    keys_count = 100
 
-#     db = ukv.DataBase()
-#     for index in range(keys_count):
-#         db.set(index, str(index).encode())
+    db = ukv.DataBase()
+    for index in range(keys_count):
+        db.set(index, str(index).encode())
 
-#     txn1 = ukv.Transaction(db)
-#     txn2 = ukv.Transaction(db)
-#     for index in range(keys_count):
-#         del txn1[index]
-#     for index in range(keys_count):
-#         del txn2[index]
+    txn1 = ukv.Transaction(db)
+    txn2 = ukv.Transaction(db)
+    for index in range(keys_count):
+        del txn1[index]
+    for index in range(keys_count):
+        del txn2[index]
 
-#     txn2.commit()
-#     with pytest.raises(Exception):
-#         txn1.commit()
+    txn2.commit()
+    with pytest.raises(Exception):
+        txn1.commit()
 
 
 def test_multiple_transactions():
@@ -169,15 +169,21 @@ def test_transaction_get_fail():
 
 # TODO: We don't have rollback to commit transactions anyway
 # def test_transactions_with_threads():
-#     keys_count = 100
+#     keys_count = 10
 #     db = ukv.DataBase()
 
 #     def set_same_value(value):
-#         with ukv.Transaction(db) as txn:
-#             for index in range(keys_count):
-#                 txn.set(index, value.encode())
+#         txn = ukv.Transaction(db)
+#         for index in range(keys_count):
+#             txn.set(index, value.encode())
+#         while True:
+#             try:
+#                 txn.commit()
+#                 break
+#             except:
+#                 pass
 
-#     threads_cnt = 64
+#     threads_cnt = 8
 #     threads = [None] * threads_cnt
 #     for index in range(threads_cnt):
 #         threads[index] = Thread(target=set_same_value, args=(str(index),))
@@ -188,3 +194,4 @@ def test_transaction_get_fail():
 
 #     for index in range(keys_count-1):
 #         assert db.get(index) == db.get(index+1)
+#         print(db.get(index))
