7d234ad056cd6f58dd67e16d772563d4034e656c
Ashot Vardanian <ashvardanian@gmail.com>
2022-06-22 19:42:15 +0400

    Implemented NumPy table exports

--- python/run.sh
+++ python/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rm -rf build && cmake . && make && pip install --upgrade --force-reinstall . && pytest -s
+rm -rf build && cmake . && make && pip install --upgrade --force-reinstall . && pytest --capture=no
