b0a42cbb43e864ffd8f35b14811956c13aeb11ec
Ishkhan Nazaryan <105867377+ishkhan42@users.noreply.github.com>
2022-06-28 16:47:11 +0400

    Fixed rocksdb build.

--- python/run.sh
+++ python/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rm -rf build && cmake . && make && pip install --upgrade --force-reinstall . && pytest --capture=no python/
+rm -rf build && rm CMakeCache.txt && cmake . && make && pip install --upgrade --force-reinstall . && pytest --capture=no python/
