9b4783ab38f4af115ad683389630a3bb6e5ede20
Ashot Vardanian <ashvardanian@gmail.com>
2022-06-29 10:46:16 +0400

    Added Integer Compression

---
+++ cmake/grpc.cmake
@@ -0,0 +1,10 @@
+# gRPC: https://grpc.io/blog/cmake-improvements/
+# https://github.com/grpc/grpc/blob/master/test/distrib/cpp/run_distrib_test_cmake_fetchcontent.sh
+
+FetchContent_Declare(
+    gRPC
+    GIT_REPOSITORY https://github.com/grpc/grpc
+    GIT_TAG v1.47.0
+    GIT_SHALLOW TRUE
+)
+FetchContent_MakeAvailable(gRPC)
---
+++ cmake/rocksdb.cmake
@@ -0,0 +1,14 @@
+# RocksDB:
+# https://github.com/facebook/rocksdb/blob/main/CMakeLists.txt
+# set(WITH_GFLAGS FALSE)
+# set(WITH_BENCHMARK_TOOLS FALSE)
+# set(WITH_CORE_TOOLS FALSE)
+# set(WITH_TOOLS FALSE)
+# set(WITH_TESTS FALSE)
+# FetchContent_Declare(
+# rocksdb
+# GIT_REPOSITORY https://github.com/facebook/rocksdb.git
+# GIT_TAG v7.3.1
+# GIT_SHALLOW TRUE
+# )
+# FetchContent_MakeAvailable(rocksdb)
