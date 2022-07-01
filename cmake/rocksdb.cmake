7e0d3823fa22896370f0559876044d48fd850157
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2022-07-01 15:37:05 +0400

    Moved RocksDB CMake

--- cmake/rocksdb.cmake
+++ cmake/rocksdb.cmake
@@ -1,14 +1,34 @@
 # RocksDB:
 # https://github.com/facebook/rocksdb/blob/main/CMakeLists.txt
-# set(WITH_GFLAGS FALSE)
-# set(WITH_BENCHMARK_TOOLS FALSE)
-# set(WITH_CORE_TOOLS FALSE)
-# set(WITH_TOOLS FALSE)
-# set(WITH_TESTS FALSE)
-# FetchContent_Declare(
-# rocksdb
-# GIT_REPOSITORY https://github.com/facebook/rocksdb.git
-# GIT_TAG v7.3.1
-# GIT_SHALLOW TRUE
-# )
-# FetchContent_MakeAvailable(rocksdb)
+
+# RocksDB:
+# https://github.com/facebook/rocksdb/blob/main/CMakeLists.txt
+FetchContent_Declare(
+    rocksdb
+    GIT_REPOSITORY https://github.com/facebook/rocksdb.git
+    GIT_TAG v7.3.1
+    GIT_SHALLOW TRUE
+)
+
+FetchContent_GetProperties(rocksdb)
+
+if(NOT rocksdb_POPULATED)
+    # Fetch the content using previously declared details
+    set(WITH_LIBURING OFF CACHE INTERNAL "")
+    set(FAIL_ON_WARNINGS OFF CACHE INTERNAL "")
+    set(WITH_BENCHMARK_TOOLS OFF CACHE INTERNAL "")
+    set(WITH_SNAPPY OFF CACHE INTERNAL "")
+    set(WITH_LZ4 OFF CACHE INTERNAL "")
+    set(WITH_GFLAGS OFF CACHE INTERNAL "")
+    set(WITH_JEMALLOC OFF CACHE INTERNAL "")
+    set(USE_RTTI 1 CACHE INTERNAL "")
+    set(PORTABLE ON CACHE INTERNAL "")
+    set(FORCE_SSE42 ON CACHE INTERNAL "")
+    set(BUILD_SHARED OFF CACHE INTERNAL "")
+    set(WITH_TESTS OFF CACHE INTERNAL "")
+    set(WITH_TOOLS OFF CACHE INTERNAL "")
+    set(CMAKE_ENABLE_SHARED OFF CACHE INTERNAL "")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -Wno-error=deprecated-copy -Wno-deprecated-copy -Wno-error=pessimizing-move -Wno-pessimizing-move")
+    FetchContent_Populate(rocksdb)
+    add_subdirectory(${rocksdb_SOURCE_DIR} ${rocksdb_BINARY_DIR} EXCLUDE_FROM_ALL)
+endif()
