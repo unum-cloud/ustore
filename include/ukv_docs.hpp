45dd48de20f5fe016136e67263af749c689e9503
Ashot Vardanian <ashvardanian@gmail.com>
2022-07-04 16:41:26 +0400

    C-layer Compilation fixes

---
+++ include/ukv_docs.hpp
@@ -0,0 +1,12 @@
+/**
+ * @file ukv_docs.hpp
+ * @author Ashot Vardanian
+ * @date 4 Jul 2022
+ * @brief C++ bindings built on top of @see "ukv_docs.h".
+ */
+
+#pragma once
+#include "ukv_docs.h"
+#include "ukv.hpp"
+
+namespace unum::ukv {} // namespace unum::ukv
