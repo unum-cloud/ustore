fda3acce875a39239ef682893d4e5db4a2898a23
Ashot Vardanian <ashvardanian@gmail.com>
2022-07-18 17:47:33 +0400

    Minor

--- python/pybind_database.cpp
+++ python/pybind_database.cpp
@@ -153,17 +153,17 @@ std::optional<py::bytes> get_item( //
  *        The most performant batch-reading method, ideal for ML.
  *
  * Contrary to most data types exposed by the Python interpreter,
- * buffers are not PyObject pointers but rather simple C structures.
+ * buffers are not @c `PyObject` pointers but rather simple C structures.
  * This allows them to be created and copied very simply.
- * When a generic wrapper around a buffer is needed, a memoryview
+ * When a generic wrapper around a buffer is needed, a @c `memoryview`
  * object can be created.
  * https://docs.python.org/3/c-api/buffer.html#buffer-structure
  *
- * @param keys      A NumPy array of keys.
- * @param valus     A buffer-protocol object, whose `shape` has 2 dims
- *                  the `itemsize` is just one byte. The first dimensions
- *                  must match with `len(keys)` and the second one must be
- *                  at least 8 for us be able to efficiently reuse that memory.
+ * @param keys       A NumPy array of keys.
+ * @param values_arr A buffer-protocol object, whose `shape` has 2 dims
+ *                   the `itemsize` is just one byte. The first dimensions
+ *                   must match with `len(keys)` and the second one must be
+ *                   at least 8 for us be able to efficiently reuse that memory.
  *
  * @param values_lengths May be nullptr.
  *
