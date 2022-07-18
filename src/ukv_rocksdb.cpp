8a753a6566fa8a2e3f3f063c404279b0a14c1fa5
davit <davit@localhost>
2022-07-18 19:01:04 +0400

    Update scan functions

--- src/ukv_rocksdb.cpp
+++ src/ukv_rocksdb.cpp
@@ -376,12 +376,16 @@ void ukv_scan( //
 
     rocksdb::ReadOptions options;
     options.fill_cache = false;
-    ukv_size_t lens_bytes = sizeof(ukv_val_len_t) * c_min_tasks_count;
-    byte_t* tape = prepare_memory(arena.output_tape, lens_bytes, c_error);
+    ukv_size_t keys_bytes = sizeof(ukv_key_t) * c_min_tasks_count;
+    ukv_size_t val_len_bytes = sizeof(ukv_val_len_t) * c_min_tasks_count;
+    byte_t* tape = prepare_memory(arena.output_tape, keys_bytes + val_len_bytes, c_error);
     if (*c_error)
         return;
-    ukv_val_len_t* lens = reinterpret_cast<ukv_val_len_t*>(tape);
-    std::fill_n(lens, c_min_tasks_count, 0);
+
+    ukv_key_t* scanned_keys = reinterpret_cast<ukv_key_t*>(tape);
+    ukv_val_len_t* scanned_lens = reinterpret_cast<ukv_val_len_t*>(tape + keys_bytes);
+    *c_found_keys = scanned_keys;
+    *c_found_lengths = scanned_lens;
 
     for (ukv_size_t i = 0; i != c_min_tasks_count; ++i) {
         scan_task_t task = tasks[i];
@@ -397,13 +401,8 @@ void ukv_scan( //
         }
         it->Seek(to_slice(task.min_key));
         for (; it->Valid() && i != task.length; i++, it->Next()) {
-            auto old_tape_len = arena.output_tape.size();
-            auto bytes_in_value = it->value().size();
-            tape = prepare_memory(arena.output_tape, old_tape_len + bytes_in_value, c_error);
-            if (*c_error)
-                return;
-            std::memcpy(tape + old_tape_len, it->value().data(), bytes_in_value);
-            lens[i] = static_cast<ukv_val_len_t>(bytes_in_value);
+            std::memcpy(&scanned_keys[i], it->key().data(), sizeof(ukv_key_t));
+            scanned_lens[i] = static_cast<ukv_val_len_t>(it->value().size());
         }
     }
 }
