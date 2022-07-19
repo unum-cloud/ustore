8a0cd8ad977129023981e319d05d730b20e6c10f
ishkhan <ishkhan@unum.cloud>
2022-07-19 17:05:08 +0400

    Updated gradle and java build process. gradle to build and link jni with different backends and pack libs with jars. Java separated backends extending from main DataBase class. And some minor build fixes .

---
+++ java/com/unum/ukv/DataBaseRocks.java
@@ -0,0 +1,34 @@
+package com.unum.ukv;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+public class DataBaseRocks extends DataBase {
+    static {
+        try {
+            System.load("/home/ishkhan/Code/UKV/build/libs/ukv/shared/rocks/libukv.so");
+        } catch (UnsatisfiedLinkError e) {
+            String paths = System.getProperty("java.library.path");
+            System.err.println("Native code library failed to load.\n" + e);
+            System.err.println("Shared libraries must be placed at:" + paths + "\n");
+            System.exit(1);
+        }
+    }
+
+    @Test
+    public void test() {
+        Context ctx = new Context("");
+        ctx.put(42, "hey".getBytes());
+        assert Arrays.equals(ctx.get(42), "hey".getBytes()) : "Received wrong value";
+
+        Transaction txn = ctx.transaction();
+        txn.put("any", 42, "meaning of life".getBytes());
+        assert Arrays.equals(txn.get("any", 42), "meaning of life".getBytes()) : "Wrong philosophy";
+        txn.commit();
+        assert Arrays.equals(ctx.get("any", 42), "meaning of life".getBytes()) : "Accepted wrong philosophy";
+
+        ctx.close();
+        System.out.println("Success!");
+    }
+}
