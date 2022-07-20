5cc76399e682ceb13156ce66c72aa1f36cafbf81
ishkhan <ishkhan@unum.cloud>
2022-07-20 16:26:00 +0400

    Configured gradle for publishing.

--- java/com/unum/ukv/DataBaseRocks.java
+++ java/com/unum/ukv/DataBaseRocks.java
@@ -1,34 +1,14 @@
 package com.unum.ukv;
 
-import org.junit.Test;
-
-import java.util.Arrays;
-
 public class DataBaseRocks extends DataBase {
-    static {
+    public static void init() {
         try {
-            System.load("/home/ishkhan/Code/UKV/build/libs/ukv/shared/rocks/libukv.so");
+            System.load("ukv/rocks/libukv.so");
         } catch (UnsatisfiedLinkError e) {
             String paths = System.getProperty("java.library.path");
             System.err.println("Native code library failed to load.\n" + e);
             System.err.println("Shared libraries must be placed at:" + paths + "\n");
             System.exit(1);
         }
-    }
-
-    @Test
-    public void test() {
-        Context ctx = new Context("");
-        ctx.put(42, "hey".getBytes());
-        assert Arrays.equals(ctx.get(42), "hey".getBytes()) : "Received wrong value";
-
-        Transaction txn = ctx.transaction();
-        txn.put("any", 42, "meaning of life".getBytes());
-        assert Arrays.equals(txn.get("any", 42), "meaning of life".getBytes()) : "Wrong philosophy";
-        txn.commit();
-        assert Arrays.equals(ctx.get("any", 42), "meaning of life".getBytes()) : "Accepted wrong philosophy";
-
-        ctx.close();
-        System.out.println("Success!");
     }
 }
