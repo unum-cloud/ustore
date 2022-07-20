56ef6aef4473d7fb8cfbf3c251be84547e3cff33
Ishkhan Nazaryan <105867377+ishkhan42@users.noreply.github.com>
2022-07-20 17:28:35 +0400

    Fixed dynamic library imports. Extracting and importing from JAR.

--- java/com/unum/ukv/DataBaseSTL.java
+++ java/com/unum/ukv/DataBaseSTL.java
@@ -3,14 +3,7 @@ package com.unum.ukv;
 
 public class DataBaseSTL extends DataBase {
     public static void init() {
-        try {
-            System.load("ukv/libukv.so");
-        } catch (UnsatisfiedLinkError e) {
-            String paths = System.getProperty("java.library.path");
-            System.err.println("Native code library failed to load.\n" + e);
-            System.err.println("Shared libraries must be placed at:" + paths + "\n");
-            System.exit(1);
-        }
+        loadLibrary("stl");
     }
 }
 
