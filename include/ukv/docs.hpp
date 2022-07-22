0eafda7493b508eba7fe722735e9708511ac1a2d
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2022-07-22 15:39:39 +0400

    Add: Initial docs_table_t view

--- include/ukv/docs.hpp
+++ include/ukv/docs.hpp
@@ -56,6 +56,14 @@ class strings_tape_iterator_t {
 };
 
 template <typename scalar_at>
+class field_cell_gt {
+    bool valid = nullptr;
+    bool converted = nullptr;
+    bool collides = nullptr;
+    scalar_at* scalars_ = nullptr;
+};
+
+template <typename scalar_at>
 class field_column_view_gt {
     std::uint8_t* validities_ = nullptr;
     std::uint8_t* conversions_ = nullptr;
@@ -63,30 +71,105 @@ class field_column_view_gt {
     scalar_at* scalars_ = nullptr;
 };
 
-template <typename scalar_at>
-class doc_row_view_gt {
+class doc_row_view_t {
+
+    strided_iterator_gt<ukv_str_view_t> fields;
+    strided_iterator_gt<ukv_type_t> types;
+
+    std::uint8_t* all_validities_ = nullptr;
+    std::uint8_t* all_conversions_ = nullptr;
+    std::uint8_t* all_collisions_ = nullptr;
+    std::uint8_t* all_scalars_ = nullptr;
+
+    located_key_t row_id_;
+    std::uint8_t row_mask_ = 0;
+    std::size_t row_idx_ = 0;
+};
+
+inline std::size_t bytes_in_bitset_column(std::size_t docs_count) noexcept {
+    return ((docs_count + 7) / 8) * 8;
+}
+
+inline std::size_t bytes_in_scalar_column(std::size_t docs_count, ukv_type_t type) noexcept {
+    switch (type) {
+    case ukv_type_bool_k: return docs_count;
+    case ukv_type_i64_k: return docs_count * 8;
+    case ukv_type_f64_k: return docs_count * 8;
+    case ukv_type_uuid_k: return docs_count * 16;
+    default: return 0;
+    }
+}
+
+class docs_table_view_t {
+
+    std::size_t docs_count_;
+    std::size_t fields_count_;
+
+    strided_iterator_gt<ukv_str_view_t> fields_;
+    strided_iterator_gt<ukv_type_t> types_;
+
     std::uint8_t* validities_ = nullptr;
     std::uint8_t* conversions_ = nullptr;
     std::uint8_t* collisions_ = nullptr;
-    scalar_at* scalars_ = nullptr;
-    std::uint8_t mask_ = 0;
+    std::uint8_t* scalars_ = nullptr;
+
+  public:
+    docs_table_view_t(std::size_t docs_count,
+                      std::size_t fields_count,
+                      strided_iterator_gt<ukv_str_view_t> fields,
+                      strided_iterator_gt<ukv_type_t> types,
+                      ukv_val_ptr_t columns_validities,
+                      ukv_val_ptr_t columns_conversions,
+                      ukv_val_ptr_t columns_collisions,
+                      ukv_val_ptr_t columns_scalars);
+
+    template <typename scalar_at>
+    field_column_view_gt<scalar_at> column(std::size_t column_idx) const noexcept {
+        std::size_t jump_bitset = bytes_in_bitset_column(docs_count_);
+        std::uint8_t* validities = validities_ + jump_bitset * column_idx;
+        std::uint8_t* conversions = conversions_ + jump_bitset * column_idx;
+        std::uint8_t* collisions = collisions_ + jump_bitset * column_idx;
+
+        std::uint8_t* scalars = scalars_;
+        if (types_.repeats())
+            scalars += bytes_in_scalar_column(docs_count_, types_[0]) * column_idx;
+        else
+            for (std::size_t i = 0; i != column_idx; ++i)
+                scalars += bytes_in_scalar_column(docs_count_, types_[i]);
+
+        return {validities, conversions, collisions, reinterpret_cast<scalar_at*>(scalars)};
+    }
+
+    std::size_t rows() const noexcept { return docs_count_; }
+    std::size_t cols() const noexcept { return fields_count_; }
 };
 
 class docs_table_t {
 
-    ukv_size_t docs_count;
-    ukv_size_t fields_count;
+    struct column_info_t {
+        ukv_str_view_t field;
+        ukv_type_t type;
+    };
+    std::vector<std::uint8_t> buffer_;
+    std::vector<column_info_t> columns_;
+    std::size_t const docs_count_ = 0;
 
-    strided_iterator_gt<ukv_collection_t> cols;
-    strided_iterator_gt<ukv_key_t> keys;
-    strided_iterator_gt<ukv_str_view_t> fields;
-    strided_iterator_gt<ukv_type_t> types;
+  public:
+    docs_table_t(std::size_t docs_count) noexcept(false) : docs_count_(docs_count) {}
+    docs_table_t& add_column(ukv_str_view_t name, ukv_type_t type) noexcept(false) {
+        auto bitset_size = bytes_in_bitset_column(docs_count_);
+        auto scalars_size = bytes_in_scalar_column(docs_count_, type);
+        buffer_.resize(buffer_.size() + bitset_size * 3 + scalars_size);
+        columns_.push_back({name, type});
+        return *this;
+    }
 
-    template <typename scalar_at>
-    field_column_view_gt<scalar_at> column(std::size_t column_idx) {}
+    void clear() noexcept {
+        columns_.clear();
+        buffer_.clear();
+    }
 
-    template <typename scalar_at>
-    field_column_view_gt<scalar_at> column(std::size_t column_idx) {}
+    docs_table_view_t view() const noexcept {}
 };
 
 } // namespace unum::ukv
