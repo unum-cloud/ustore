7612bf5515613b5a4f01362b6b21b864e6046248
ishkhan <ishkhan@unum.cloud>
2022-08-01 13:06:20 +0400

    Golang: fixed compilation, separated internal implementation, to be more modular.

---
+++ golang/level/ukv.go
@@ -0,0 +1,31 @@
+package ukv
+
+/*
+#cgo LDFLAGS: -L${SRCDIR}/../../lib -lukv_leveldb -lleveldb -lstdc++
+#cgo CFLAGS: -g -Wall -I${SRCDIR}/../../include
+
+#include "ukv/db.h"
+#include <stdlib.h>
+*/
+import "C"
+import (
+	u "github.com/unum-cloud/UKV/golang/internal"
+)
+
+type Level struct {
+	u.DataBase
+}
+
+func CreateDB() Level {
+	backend := u.BackendInterface{
+		UKV_error_free:      C.ukv_error_free,
+		UKV_arena_free:      C.ukv_arena_free,
+		UKV_open:            C.ukv_open,
+		UKV_free:            C.ukv_free,
+		UKV_read:            C.ukv_read,
+		UKV_write:           C.ukv_write,
+		UKV_val_len_missing: u.UKV_val_len_t(C.ukv_val_len_missing_k)}
+
+	db := Level{DataBase: u.DataBase{Backend: backend}}
+	return db
+}
