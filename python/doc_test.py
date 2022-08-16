65825bd3620afd966921b7bc26797c98a818c1f7
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2022-08-16 19:18:34 +0400

    Add: Python Tabular examples

--- python/doc_test.py
+++ python/doc_test.py
@@ -4,39 +4,61 @@ import ukv.stl as ukv
 
 
 def only_json(col):
+    docs = col.docs
 
-    original = json.dumps({
+    original = {
         'US': 'US',
         'Japan': '日本',
-        'Armenia': 'Հայաստան'
-    }).encode()
-
-    col[1] = original
+        'Armenia': 'Հայաստան',
+    }
+    docs[1] = original
     assert 1 in col
 
-    retrieved = json.loads(col[1].decode())
+    retrieved = docs[1]
     assert original == retrieved, 'Failed to recover document'
 
-    # Batch lookups, via lists AND via tuples
-    col[(1, 2, 3, 4)]
-    col[[1, 2, 3, 4]]
+    docs[1].patch([
+        {'op': 'add', 'path': '/swag', 'value': 'maximum'},
+        # {'op': 'replace', 'path': '/baz', 'value': 'boo'},
+        # {'op': 'remove', 'path': '/foo'}
+    ])
 
-    # Batch checks and deletions
-    (1, 2, 3, 4) in col
-    del col[(1, 2, 3, 4)]
+    docs[1].merge({
+        '/swag': 'ultimate'
+    })
 
     # Batch field lookup
     # Supporting JSON-Pointers
-    col[1 / '/US']
-    col[1]['US']
-    col[(1, 2, 3)][('US', 'Phone', 'Name')]
+    docs['1/US']
 
-    # Listing a range of documents
-    col[1:]
-    col[1:].keys()
-    col[1:1000]
+
+def only_table(col):
+    docs = col.docs
+    docs[1] = {'name': 'Lex', 'lastname': 'Fridman', 'tweets': 2221}
+    docs[2] = {'name': 'Andrew', 'lastname': 'Huberman', 'tweets': 3935}
+    docs[3] = {'name': 'Joe', 'lastname': 'Rogan', 'tweets': 45900}
+
+    table = col.table
+
+    # Single-type tables
+    assert table[['tweets']].astype('int32').loc[1, 2, 3] \
+        == [2221, 3935, 45900]
+    assert table[['name']].astype('string').head(4) \
+        == ['Lex', 'Andrew', 'Joe', None]
+    assert table[['tweets']].astype('string').head(4) \
+        == ['2221', '3935', '45900', None]
+
+    results = table.astype({
+        'name': 'string',
+        'tweets': 'int64'
+    }).loc[1, 2, 3]
+    assert results[0] == ['Lex', 'Andrew', 'Joe']
+    assert results[1] == [2221, 3935, 45900]
+
+    results = table.loc[100:].head(10)
 
 
 def testing():
-    main = ukv.DataBase().main
-    only_json(main)
+    db = ukv.DataBase()
+    only_json(db.main)
+    only_table(db.main)
