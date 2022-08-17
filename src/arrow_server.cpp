c4995cc1abf9f2fd5a33b3c17c384c9f32b5c9d9
Ashot Vardanian <ashvardanian@gmail.com>
2022-08-17 20:57:49 +0400

    Add: Arrow server as both internal and Flight RPC

--- src/arrow_server.cpp
+++ src/arrow_server.cpp
@@ -9,19 +9,19 @@
  * https://arrow.apache.org/cookbook/cpp/flight.html
  */
 
-#include <arrow/buffer.h>
-#include <arrow/filesystem/filesystem.h>
-#include <arrow/filesystem/localfs.h>
-#include <arrow/flight/client.h>
-#include <arrow/flight/server.h>
-#include <arrow/pretty_print.h>
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wall"
+#pragma GCC diagnostic ignored "-Wextra"
+#include <arrow/type.h>
 #include <arrow/result.h>
 #include <arrow/status.h>
+#include <arrow/buffer.h>
 #include <arrow/table.h>
-#include <arrow/type.h>
-
-#include <parquet/arrow/reader.h>
-#include <parquet/arrow/writer.h>
+#include <arrow/pretty_print.h>
+#include <arrow/flight/client.h>
+#include <arrow/flight/server.h>
+#include <arrow/c/bridge.h> // `ExportSchema`
+#pragma GCC diagnostic pop
 
 #include <algorithm>
 #include <memory>
@@ -29,168 +29,203 @@
 #include <vector>
 #include <iostream>
 
-#include "ukv/cpp/arrow.hpp"
+#define ARROW_C_DATA_INTERFACE 1
+#define ARROW_C_STREAM_INTERFACE 1
+#include "ukv/arrow.h"
+#include "ukv/cpp/db.hpp"
 
-using namespace arrow::flight;
-using namespace arrow;
+namespace arf = arrow::flight;
+namespace ar = arrow;
 
-class UKVService : public arrow::flight::FlightServerBase {
+using namespace unum::ukv;
+using namespace unum;
+
+expected_gt<std::size_t> column_idx(ArrowSchema* schema_ptr, std::string_view name) {
+    auto begin = schema_ptr->children;
+    auto end = begin + schema_ptr->n_children;
+    auto it = std::find_if(begin, end, [=](ArrowSchema* column_schema) {
+        return std::string_view {column_schema->name} == name;
+    });
+    if (it == end)
+        return status_t {"Column not found!"};
+    return static_cast<std::size_t>(it - begin);
+}
+
+bool validate_column_cols(ArrowSchema* schema_ptr, ArrowArray* column_ptr) {
+    if (schema_ptr->format != ukv_type_to_arrow_format(ukv_type<ukv_col_t>()))
+        return false;
+    if (column_ptr->null_count != 0)
+        return false;
+    return true;
+}
+
+bool validate_column_keys(ArrowSchema* schema_ptr, ArrowArray* column_ptr) {
+    if (schema_ptr->format != ukv_type_to_arrow_format(ukv_type<ukv_key_t>()))
+        return false;
+    if (column_ptr->null_count != 0)
+        return false;
+    return true;
+}
+
+bool validate_column_vals(ArrowSchema* schema_ptr, ArrowArray* column_ptr) {
+    if (schema_ptr->format != ukv_type_to_arrow_format(ukv_type<value_view_t>()))
+        return false;
+    if (column_ptr->null_count != 0)
+        return false;
+    return true;
+}
+
+class UKVService : public arf::FlightServerBase {
   public:
-    const arrow::flight::ActionType kActionDropDataset {"drop_dataset", "Delete a dataset."};
+    inline static arf::ActionType const kActionColOpen {"col_open", "Find a collection descriptor by name."};
+    inline static arf::ActionType const kActionColRemove {"col_remove", "Delete a named collection."};
+    inline static arf::ActionType const kActionColList {"col_list", "Lists all named collections."};
+    inline static arf::ActionType const kActionTxnBegin {"txn_begin", "Starts an ACID transaction and returns its ID."};
+    inline static arf::ActionType const kActionTxnCommit {"txn_commit", "Commit a previously started transaction."};
 
-    explicit UKVService(std::shared_ptr<arrow::fs::FileSystem> root) : root_(std::move(root)) {}
+    inline static std::string const kOpRead = "read";
+    inline static std::string const kOpWrite = "write";
+    inline static std::string const kOpScan = "scan";
+    inline static std::string const kOpSize = "size";
 
-    arrow::Status ListFlights( //
-        arrow::flight::ServerCallContext const&,
-        arrow::flight::Criteria const*,
-        std::unique_ptr<arrow::flight::FlightListing>* listings) override {
+    inline static std::string const kArgCols = "cols";
+    inline static std::string const kArgKeys = "keys";
+    inline static std::string const kArgVals = "vals";
+    inline static std::string const kArgFields = "fields";
 
-        arrow::fs::FileSelector selector;
-        selector.base_dir = "/";
-        ARROW_ASSIGN_OR_RAISE(auto listing, root_->GetFileInfo(selector));
+    db_t db_;
 
-        std::vector<arrow::flight::FlightInfo> flights;
-        for (const auto& file_info : listing) {
-            if (!file_info.IsFile() || file_info.extension() != "parquet")
-                continue;
-            ARROW_ASSIGN_OR_RAISE(auto info, MakeFlightInfo(file_info));
-            flights.push_back(std::move(info));
+    UKVService(db_t&& db) noexcept : db_(std::move(db)) {}
+
+    ar::Status ListActions( //
+        arf::ServerCallContext const&,
+        std::vector<arf::ActionType>* actions) override {
+        *actions = {kActionColOpen, kActionColRemove, kActionColList, kActionTxnBegin, kActionTxnCommit};
+        return ar::Status::OK();
+    }
+
+    ar::Status ListFlights( //
+        arf::ServerCallContext const&,
+        arf::Criteria const*,
+        std::unique_ptr<arf::FlightListing>*) override {
+        return ar::Status::OK();
+    }
+
+    ar::Status GetFlightInfo( //
+        arf::ServerCallContext const&,
+        arf::FlightDescriptor const&,
+        std::unique_ptr<arf::FlightInfo>*) override {
+        // ARROW_ASSIGN_OR_RAISE(auto file_info, FileInfoFromDescriptor(descriptor));
+        // ARROW_ASSIGN_OR_RAISE(auto flight_info, MakeFlightInfo(file_info));
+        // *info = std::make_unique<arf::FlightInfo>(std::move(flight_info));
+        return ar::Status::OK();
+    }
+
+    ar::Status GetSchema( //
+        arf::ServerCallContext const&,
+        arf::FlightDescriptor const&,
+        std::unique_ptr<arf::SchemaResult>*) override {
+        return ar::Status::OK();
+    }
+
+    ar::Status DoAction( //
+        arf::ServerCallContext const&,
+        arf::Action const& action,
+        std::unique_ptr<arf::ResultStream>*) override {
+
+        if (action.type == kActionColOpen.type) {
+        }
+        return ar::Status::NotImplemented("Unknown action type: ", action.type);
+    }
+
+    ar::Status DoExchange( //
+        arf::ServerCallContext const& context,
+        std::unique_ptr<arf::FlightMessageReader> request_ptr,
+        std::unique_ptr<arf::FlightMessageWriter> response_ptr) {
+
+        arf::FlightMessageReader& request = *request_ptr;
+        arf::FlightMessageWriter& response = *response_ptr;
+        arf::FlightDescriptor const& desc = request.descriptor();
+        std::string const& cmd = desc.cmd;
+
+        std::shared_ptr<ar::Table> const& full = request.ToTable().ValueOrDie();
+        std::vector<std::shared_ptr<ar::RecordBatch>> const& batches = request.ToRecordBatches().ValueOrDie();
+
+        if (desc.cmd == kOpRead) {
+        }
+        return ar::Status::OK();
+    }
+
+    ar::Status DoPut( //
+        arf::ServerCallContext const& context,
+        std::unique_ptr<arf::FlightMessageReader> request_ptr,
+        std::unique_ptr<arf::FlightMetadataWriter> response_ptr) override {
+
+        arf::FlightMessageReader& request = *request_ptr;
+        arf::FlightMetadataWriter& response = *response_ptr;
+        arf::FlightDescriptor const& desc = request.descriptor();
+        std::string const& cmd = desc.cmd;
+        std::shared_ptr<ar::Schema> const& schema_ptr = request.GetSchema().ValueOrDie();
+        ArrowSchema schema_c;
+        auto _ = ar::ExportSchema(*schema_ptr, &schema_c);
+
+        std::optional<std::size_t> idx_cols = column_idx(&schema_c, kArgCols);
+        std::optional<std::size_t> idx_keys = column_idx(&schema_c, kArgKeys);
+        std::optional<std::size_t> idx_vals = column_idx(&schema_c, kArgVals);
+
+        if (desc.cmd == kOpWrite) {
+
+            while (true) {
+                arf::FlightStreamChunk const& chunk = request.Next().ValueOrDie();
+                std::shared_ptr<ar::RecordBatch> const& batch_ptr = chunk.data;
+                if (!batch_ptr)
+                    break;
+
+                ArrowArray batch_c;
+                auto _ = ar::ExportRecordBatch(*batch_ptr, &batch_c, nullptr);
+
+                ArrowArray* keys_c = batch_c.children[*idx_keys];
+                ArrowArray* vals_c = batch_c.children[*idx_vals];
+                status_t status;
+                arena_t arena(db_);
+                ukv_write( //
+                    db_,
+                    nullptr,
+                    keys_c->length,
+                    nullptr,
+                    0,
+                    (ukv_key_t const*)keys_c->buffers[1],
+                    sizeof(ukv_key_t),
+                    (ukv_val_ptr_t const*)&vals_c->buffers[2],
+                    0,
+                    (ukv_val_len_t const*)vals_c->buffers[1],
+                    sizeof(ukv_val_len_t),
+                    nullptr,
+                    0,
+                    ukv_options_default_k,
+                    arena.member_ptr(),
+                    status.member_ptr());
+            }
         }
 
-        *listings =
-            std::unique_ptr<arrow::flight::FlightListing>(new arrow::flight::SimpleFlightListing(std::move(flights)));
-        return arrow::Status::OK();
+        return ar::Status::OK();
     }
 
-    arrow::Status GetFlightInfo( //
-        arrow::flight::ServerCallContext const&,
-        arrow::flight::FlightDescriptor const& descriptor,
-        std::unique_ptr<arrow::flight::FlightInfo>* info) override {
-
-        ARROW_ASSIGN_OR_RAISE(auto file_info, FileInfoFromDescriptor(descriptor));
-        ARROW_ASSIGN_OR_RAISE(auto flight_info, MakeFlightInfo(file_info));
-        *info = std::unique_ptr<arrow::flight::FlightInfo>(new arrow::flight::FlightInfo(std::move(flight_info)));
-        return arrow::Status::OK();
+    ar::Status DoGet( //
+        arf::ServerCallContext const&,
+        arf::Ticket const&,
+        std::unique_ptr<arf::FlightDataStream>*) override {
+        return ar::Status::OK();
     }
-
-    arrow::Status GetSchema( //
-        arrow::flight::ServerCallContext const& context,
-        arrow::flight::FlightDescriptor const& request,
-        std::unique_ptr<arrow::flight::SchemaResult>* schema) override {
-        return arrow::Status::OK();
-    }
-
-    arrow::Status DoPut( //
-        arrow::flight::ServerCallContext const&,
-        std::unique_ptr<arrow::flight::FlightMessageReader> reader,
-        std::unique_ptr<arrow::flight::FlightMetadataWriter>) override {
-
-        // ARROW_ASSIGN_OR_RAISE(arrow::Result<std::shared_ptr<arrow::Schema>> schema, reader->GetSchema());
-        // ARROW_ASSIGN_OR_RAISE(arrow::Result<arrow::flight::FlightStreamChunk> chunk, reader->Next());
-
-        ARROW_ASSIGN_OR_RAISE(auto file_info, FileInfoFromDescriptor(reader->descriptor()));
-        ARROW_ASSIGN_OR_RAISE(auto sink, root_->OpenOutputStream(file_info.path()));
-        ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::Table> table, reader->ToTable());
-
-        ARROW_RETURN_NOT_OK(
-            parquet::arrow::WriteTable(*table, arrow::default_memory_pool(), sink, /*chunk_size=*/65536));
-        return arrow::Status::OK();
-    }
-
-    arrow::Status DoGet( //
-        arrow::flight::ServerCallContext const&,
-        arrow::flight::Ticket const& request,
-        std::unique_ptr<arrow::flight::FlightDataStream>* stream) override {
-
-        ARROW_ASSIGN_OR_RAISE(auto input, root_->OpenInputFile(request.ticket));
-        std::unique_ptr<parquet::arrow::FileReader> reader;
-        ARROW_RETURN_NOT_OK(parquet::arrow::OpenFile(std::move(input), arrow::default_memory_pool(), &reader));
-
-        std::shared_ptr<arrow::Table> table;
-        ARROW_RETURN_NOT_OK(reader->ReadTable(&table));
-        // Note that we can't directly pass TableBatchReader to
-        // RecordBatchStream because TableBatchReader keeps a non-owning
-        // reference to the underlying Table, which would then get freed
-        // when we exit this function
-        std::vector<std::shared_ptr<arrow::RecordBatch>> batches;
-        arrow::TableBatchReader batch_reader(*table);
-        ARROW_ASSIGN_OR_RAISE(batches, batch_reader.ToRecordBatches());
-
-        ARROW_ASSIGN_OR_RAISE(auto owning_reader, arrow::RecordBatchReader::Make(std::move(batches), table->schema()));
-        *stream = std::unique_ptr<arrow::flight::FlightDataStream>(new arrow::flight::RecordBatchStream(owning_reader));
-
-        return arrow::Status::OK();
-    }
-
-    arrow::Status ListActions( //
-        arrow::flight::ServerCallContext const&,
-        std::vector<arrow::flight::ActionType>* actions) override {
-
-        *actions = {kActionDropDataset};
-        return arrow::Status::OK();
-    }
-
-    arrow::Status DoAction( //
-        arrow::flight::ServerCallContext const&,
-        arrow::flight::Action const& action,
-        std::unique_ptr<arrow::flight::ResultStream>* result) override {
-
-        if (action.type == kActionDropDataset.type) {
-            *result = std::unique_ptr<arrow::flight::ResultStream>(new arrow::flight::SimpleResultStream({}));
-            return DoActionDropDataset(action.body->ToString());
-        }
-        return arrow::Status::NotImplemented("Unknown action type: ", action.type);
-    }
-
-  private:
-    arrow::Result<arrow::flight::FlightInfo> MakeFlightInfo(arrow::fs::FileInfo const& file_info) {
-        ARROW_ASSIGN_OR_RAISE(auto input, root_->OpenInputFile(file_info));
-        std::unique_ptr<parquet::arrow::FileReader> reader;
-        ARROW_RETURN_NOT_OK(parquet::arrow::OpenFile(std::move(input), arrow::default_memory_pool(), &reader));
-
-        std::shared_ptr<arrow::Schema> schema;
-        ARROW_RETURN_NOT_OK(reader->GetSchema(&schema));
-
-        auto descriptor = arrow::flight::FlightDescriptor::Path({file_info.base_name()});
-
-        arrow::flight::FlightEndpoint endpoint;
-        endpoint.ticket.ticket = file_info.base_name();
-        arrow::flight::Location location;
-        ARROW_ASSIGN_OR_RAISE(location, arrow::flight::Location::ForGrpcTcp("localhost", port()));
-        endpoint.locations.push_back(location);
-
-        int64_t total_records = 0; // reader->parquet_reader()->metadata()->num_rows();
-        int64_t total_bytes = file_info.size();
-
-        return arrow::flight::FlightInfo::Make(*schema, descriptor, {endpoint}, total_records, total_bytes);
-    }
-
-    arrow::Result<arrow::fs::FileInfo> FileInfoFromDescriptor(arrow::flight::FlightDescriptor const& descriptor) {
-        if (descriptor.type != arrow::flight::FlightDescriptor::PATH) {
-            return arrow::Status::Invalid("Must provide PATH-type FlightDescriptor");
-        }
-        else if (descriptor.path.size() != 1) {
-            return arrow::Status::Invalid("Must provide PATH-type FlightDescriptor with one path component");
-        }
-        return root_->GetFileInfo(descriptor.path[0]);
-    }
-
-    arrow::Status DoActionDropDataset(std::string const& key) { return root_->DeleteFile(key); }
-
-    std::shared_ptr<arrow::fs::FileSystem> root_;
 };
 
-arrow::Status run_server() {
-    auto fs = std::make_shared<arrow::fs::LocalFileSystem>();
-    ARROW_RETURN_NOT_OK(fs->CreateDir("./flight_datasets/"));
-    ARROW_RETURN_NOT_OK(fs->DeleteDirContents("./flight_datasets/"));
-    auto root = std::make_shared<arrow::fs::SubTreeFileSystem>("./flight_datasets/", fs);
+ar::Status run_server() {
+    db_t db;
+    db.open().throw_unhandled();
 
-    arrow::flight::Location server_location;
-    ARROW_ASSIGN_OR_RAISE(server_location, arrow::flight::Location::ForGrpcTcp("0.0.0.0", 38709));
-
-    arrow::flight::FlightServerOptions options(server_location);
-    auto server = std::unique_ptr<arrow::flight::FlightServerBase>(new UKVService(std::move(root)));
+    arf::Location server_location = arf::Location::ForGrpcTcp("0.0.0.0", 38709).ValueOrDie();
+    arf::FlightServerOptions options(server_location);
+    auto server = std::make_unique<UKVService>(std::move(db));
     ARROW_RETURN_NOT_OK(server->Init(options));
     std::cout << "Listening on port " << server->port() << std::endl;
     return server->Serve();
