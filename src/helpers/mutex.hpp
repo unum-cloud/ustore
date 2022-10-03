f9abb66006689956798c7289ebd293bbf8fc2f94
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2022-10-03 09:39:54 +0400

    Add: Placeholder for shared_mutex

---
+++ src/helpers/mutex.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+namespace unum::ukv {
+
+/**
+ * @brief TODO: A hybrid `shared_mutex` with upgrade and downgrade ability.
+ *
+ * @section Other Implementations
+ * https://github.com/ssteinberg/shared_futex
+ * https://github.com/AlexeyAB/object_threadsafe
+ * https://github.com/yohhoy/yamc
+ *
+ */
+struct shared_mutex_t {};
+
+} // namespace unum::ukv
