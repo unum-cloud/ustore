20cc0581818c72144a4ea67a8ea9a1a6811cc2a0
Davit Vardanyan <78792753+davvard@users.noreply.github.com>
2022-10-10 16:56:36 +0400

    Update stress test

--- tests/transaction_stress_test.cpp
+++ tests/transaction_stress_test.cpp
@@ -81,14 +81,7 @@ struct operation_gt {
     std::array<ukv_key_t, array_size_ak> keys;
     std::array<std::uint64_t, array_size_ak> values;
 
-    inline operation_gt(op_code_t op_type,
-                        std::size_t op_count,
-                        std::array<ukv_key_t, array_size_ak>&& op_keys,
-                        std::array<std::uint64_t, array_size_ak>&& op_values)
-        : type(op_type), count(op_count), keys(std::move(op_keys)), values((std::move(op_values))) {}
-
-    inline operation_gt(op_code_t op_type, std::size_t op_count, std::array<ukv_key_t, array_size_ak>&& op_keys)
-        : type(op_type), count(op_count), keys(std::move(op_keys)) {}
+    inline operation_gt(op_code_t op_type, std::size_t op_count) : type(op_type), count(op_count) {}
 };
 
 template <typename element_at, std::size_t arr_size>
@@ -123,15 +116,14 @@ void serializability_mix() {
         };
 
         for (std::size_t txn_idx = 0; txn_idx != txn_cnt_ak; ++txn_idx) {
-            std::array<ukv_key_t, max_op_per_txn_ak> keys;
-            std::array<std::uint64_t, max_op_per_txn_ak> values;
             std::size_t op_cnt = dist(gen);
-            random_fill(keys, op_cnt);
-            random_fill(values, op_cnt);
+            operation_t operation(operation_t::insert_k, op_cnt);
+            random_fill(operation.keys, op_cnt);
+            random_fill(operation.values, op_cnt);
 
             transaction_t txn = db.transact().throw_or_release();
-            auto txn_ref = txn[strided_range_gt<ukv_key_t>(keys.data(), op_cnt)];
-            auto vals_begin = reinterpret_cast<ukv_bytes_ptr_t>(values.data());
+            auto txn_ref = txn[strided_range_gt<ukv_key_t>(operation.keys.data(), op_cnt)];
+            auto vals_begin = reinterpret_cast<ukv_bytes_ptr_t>(operation.values.data());
             contents.contents_begin = {&vals_begin, 0};
             status_t status = txn_ref.assign(contents);
             if (!status)
@@ -140,10 +132,9 @@ void serializability_mix() {
             time_point_t time = std::chrono::high_resolution_clock::now().time_since_epoch().count();
             if (!status)
                 continue;
-            operation_t operation(operation_t::insert_k, op_cnt, std::move(keys), std::move(values));
 
             mutex.lock();
-            operations.push_back(std::pair<time_point_t, operation_t>(time, operation));
+            operations.emplace_back(time, std::move(operation));
             mutex.unlock();
         }
     };
@@ -151,11 +142,11 @@ void serializability_mix() {
     auto task_remove = [&]() {
         for (std::size_t txn_idx = 0; txn_idx != txn_cnt_ak; ++txn_idx) {
             std::size_t op_cnt = dist(gen);
-            std::array<ukv_key_t, max_op_per_txn_ak> keys;
-            random_fill(keys, op_cnt);
+            operation_t operation(operation_t::remove_k, op_cnt);
+            random_fill(operation.keys, op_cnt);
 
             transaction_t txn = db.transact().throw_or_release();
-            auto txn_ref = txn[strided_range_gt<ukv_key_t>(keys.data(), op_cnt)];
+            auto txn_ref = txn[strided_range_gt<ukv_key_t>(operation.keys.data(), op_cnt)];
             status_t status = txn_ref.erase();
             if (!status)
                 continue;
@@ -163,10 +154,9 @@ void serializability_mix() {
             time_point_t time = std::chrono::high_resolution_clock::now().time_since_epoch().count();
             if (!status)
                 continue;
-            operation_t operation(operation_t::remove_k, op_cnt, std::move(keys));
 
             mutex.lock();
-            operations.push_back(std::pair<time_point_t, operation_t>(time, operation));
+            operations.emplace_back(time, std::move(operation));
             mutex.unlock();
         }
     };
@@ -174,12 +164,11 @@ void serializability_mix() {
     auto task_select = [&]() {
         for (std::size_t txn_idx = 0; txn_idx != txn_cnt_ak; ++txn_idx) {
             std::size_t op_cnt = dist(gen);
-            std::array<ukv_key_t, max_op_per_txn_ak> keys;
-            std::array<std::uint64_t, max_op_per_txn_ak> values;
-            random_fill(keys, op_cnt);
+            operation_t operation(operation_t::select_k, op_cnt);
+            random_fill(operation.keys, op_cnt);
 
             transaction_t txn = db.transact().throw_or_release();
-            auto txn_ref = txn[strided_range_gt<ukv_key_t>(keys.data(), op_cnt)];
+            auto txn_ref = txn[strided_range_gt<ukv_key_t>(operation.keys.data(), op_cnt)];
             auto const& retrieved = txn_ref.value().throw_or_release();
             status_t status = txn.commit();
             time_point_t time = std::chrono::high_resolution_clock::now().time_since_epoch().count();
@@ -190,14 +179,13 @@ void serializability_mix() {
             for (std::size_t i = 0; i != op_cnt; ++i, ++it) {
                 value_view_t val_view = *it;
                 if (val_view)
-                    std::memcpy((void*)&values[i], (const void*)val_view.data(), sizeof(std::uint64_t));
+                    std::memcpy((void*)&operation.values[i], (const void*)val_view.data(), sizeof(std::uint64_t));
                 else
-                    values[i] = 0;
+                    operation.values[i] = 0;
             }
-            operation_t operation(operation_t::select_k, op_cnt, std::move(keys), std::move(values));
 
             mutex.lock();
-            operations.push_back(std::pair<time_point_t, operation_t>(time, operation));
+            operations.emplace_back(time, std::move(operation));
             mutex.unlock();
         }
     };
