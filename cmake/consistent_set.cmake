0a41a7007c8618e7a530c3d21104c82fd8c45a81
Ashot Vardanian <ashvardanian@gmail.com>
2022-10-12 21:31:08 +0400

    Fix: Stable cmake includes

---
+++ cmake/consistent_set.cmake
@@ -0,0 +1,10 @@
+include(FetchContent)
+FetchContent_Declare(
+    consistent_set
+    GIT_REPOSITORY https://github.com/ashvardanian/consistent_set
+    GIT_TAG main
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+)
+FetchContent_MakeAvailable(consistent_set)
+include_directories(${consistent_set_SOURCE_DIR})
