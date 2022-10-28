8290e26c5b502ea2e25a9d96a2c399e22204dd78
Mesrop <mesrop.gevorgyan.am@gmail.com>
2022-10-28 09:39:49 +0400

    Transactions: Add field to get stage and commit sequence numbers

--- src/engine_rocksdb.cpp
+++ src/engine_rocksdb.cpp
@@ -661,10 +661,14 @@ void ukv_transaction_init(ukv_transaction_init_t* c_ptr) {
 void ukv_transaction_commit(ukv_transaction_commit_t* c_ptr) {
 
     ukv_transaction_commit_t& c = *c_ptr;
-    if (!c.transaction)
-        return;
+    return_if_error(c.transaction && c.seq_number, c.error, args_combo_k, "Need transaction and outputs!");
+
     rocks_txn_t& txn = *reinterpret_cast<rocks_txn_t*>(c.transaction);
     rocks_status_t status = txn.Commit();
+
+    // TODO: Set sequence number
+    *c.seq_number = 0;
+
     export_error(status, c.error);
 }
 
