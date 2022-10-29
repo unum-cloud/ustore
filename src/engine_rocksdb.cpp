7199e658de40bd182c87e8e23533a2b36040158e
Mesrop <mesrop.gevorgyan.am@gmail.com>
2022-10-29 09:29:12 +0400

    Minor

--- src/engine_rocksdb.cpp
+++ src/engine_rocksdb.cpp
@@ -661,10 +661,11 @@ void ukv_transaction_init(ukv_transaction_init_t* c_ptr) {
 void ukv_transaction_commit(ukv_transaction_commit_t* c_ptr) {
 
     ukv_transaction_commit_t& c = *c_ptr;
+    if (!c.transaction)
+        return;
 
     rocks_txn_t& txn = *reinterpret_cast<rocks_txn_t*>(c.transaction);
     rocks_status_t status = txn.Commit();
-
     export_error(status, c.error);
 }
 
