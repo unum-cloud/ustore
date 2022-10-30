2e4ebed483b79f8f8048ad5976eace3ac1d9ca61
Ashot Vardanian <ashvardanian@gmail.com>
2022-10-30 19:06:29 +0400

    Fix: Ordering in flat limited priority queue

--- src/helpers/limited_priority_queue.hpp
+++ src/helpers/limited_priority_queue.hpp
@@ -28,7 +28,7 @@ class limited_priority_queue_gt {
     std::size_t length_ = 0;
     std::size_t capacity_ = 0;
 
-    static bool less_important(element_t const& a, element_t const& b) noexcept { return comparator_t {}(a, b); }
+    static bool higher_priority(element_t const& a, element_t const& b) noexcept { return comparator_t {}(b, a); }
 
   public:
     limited_priority_queue_gt(limited_priority_queue_gt const&) = delete;
@@ -53,10 +53,10 @@ class limited_priority_queue_gt {
         length_ = 0;
     }
 
-    ptrc_t data() const noexcept { return ptr_; }
     ptr_t data() noexcept { return ptr_; }
-    ptr_t begin() const noexcept { return reinterpret_cast<ptr_t>(ptr_); }
-    ptr_t end() const noexcept { return begin() + length_; }
+    ptrc_t data() const noexcept { return ptr_; }
+    ptrc_t begin() const noexcept { return ptr_; }
+    ptrc_t end() const noexcept { return begin() + length_; }
     element_t& operator[](std::size_t i) noexcept { return ptr_[i]; }
     std::size_t size() const noexcept { return length_; }
     std::size_t capacity() const noexcept { return capacity_; }
@@ -69,23 +69,30 @@ class limited_priority_queue_gt {
      * @return false If the priority of the input was to low to keep it.
      */
     bool push(element_t element) {
-        if (length_ >= capacity_) {
-            // Find the optimal place within the container,
-            // where this entry belongs.
-            auto element_ptr = std::lower_bound(ptr_, ptr_ + length_, element, &less_important);
-            if (element_ptr == end())
-                return false;
 
+        // Find the optimal place within the container,
+        // where this entry belongs.
+        auto end = ptr_ + length_;
+        auto element_ptr = std::lower_bound(ptr_, end, element, &higher_priority);
+        if (element_ptr == end) {
+            if (length_ < capacity_) {
+                new (end) element_t(std::move(element));
+                ++length_;
+                return true;
+            }
+            else
+                return false;
+        }
+        else {
+            if (length_ == capacity_)
+                std::destroy_at(ptr_ + capacity_ - 1);
             auto source_ptr = element_ptr;
-            for (; source_ptr + 1 != end(); ++source_ptr)
+            for (; source_ptr + 1 != end; ++source_ptr)
                 source_ptr[1] = std::move(source_ptr[0]);
             *element_ptr = std::move(element);
+            length_ += length_ < capacity_;
             return true;
         }
-
-        new (ptr_ + length_) element_t(std::move(element));
-        ++length_;
-        return true;
     }
 };
 
