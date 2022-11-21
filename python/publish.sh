fa6039e9f5f3ce0fb87fc56ddd9d771ac3a283ca
Ishkhan Nazaryan <105867377+ishkhan42@users.noreply.github.com>
2022-11-21 13:27:37 +0000

    Test: python modules workflow.

--- python/publish.sh
+++ python/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python setup.py bdist_wheel --universal --build-number=$(date -d now '+%s') &&
+python3.9 setup.py bdist_wheel --universal --build-number=$(date -d now '+%s') &&
 twine upload --repository-url=https://test.pypi.org/legacy/ dist/* -u $1 -p $2 --verbose 
