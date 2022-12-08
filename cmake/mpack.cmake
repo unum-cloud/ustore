ab7b313a63b9278d56bc16f2cf36981e1dc49b57
Violeta Stepanyan <82702795+VioletaStepanyan@users.noreply.github.com>
2022-12-08 15:17:44 +0400

    Fix: build: Include correction.

--- cmake/mpack.cmake
+++ cmake/mpack.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
     mpack
     GIT_REPOSITORY https://github.com/unum-cloud/mpack.git
-    GIT_TAG v1.1
+    GIT_TAG origin/develop
 )
 FetchContent_MakeAvailable(mpack)
 include_directories(${mpack_SOURCE_DIR}/src/mpack)
