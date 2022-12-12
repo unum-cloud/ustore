c056df1516bdd2a9a678160ae0aefe399f23ac6c
Ishkhan Nazaryan <105867377+ishkhan42@users.noreply.github.com>
2022-12-12 15:27:28 +0400

    Fix: publishing to main pypi

--- python/publish.sh
+++ python/publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 cibuildwheel --platform linux &&
-    twine upload --repository-url=https://test.pypi.org/legacy/ wheelhouse/*.whl -u $1 -p $2 --verbose
+    twine upload wheelhouse/*.whl -u $1 -p $2 --verbose
