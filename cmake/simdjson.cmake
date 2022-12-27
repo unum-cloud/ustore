6f1793857d46c34a36368811d306214014893cb4
Darvin Harutyunyan <12006323+DarvinHarutyunyan@users.noreply.github.com>
2022-12-27 14:07:14 +0400

    Make: By default SIMDJSON_ENABLE_THREADS is ON

--- cmake/simdjson.cmake
+++ cmake/simdjson.cmake
@@ -4,14 +4,5 @@ FetchContent_Declare(
     GIT_REPOSITORY https://github.com/simdjson/simdjson
     GIT_TAG v3.0.0
 )
-
-FetchContent_GetProperties(simdjson)
-if(NOT simdjson_POPULATED)
-    # Fetch the content using previously declared details
-    set(SIMDJSON_ENABLE_THREADS ON CACHE INTERNAL "")
-    FetchContent_Populate(simdjson)
-    add_subdirectory(${simdjson_SOURCE_DIR} ${simdjson_BINARY_DIR} EXCLUDE_FROM_ALL)
-endif()
-
 FetchContent_MakeAvailable(simdjson)
 include_directories(${simdjson_SOURCE_DIR}/include)
