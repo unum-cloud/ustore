db8a4bae23a01ec0937b968f2dbbc7d712a53b50
Ashot Vardanian <1983160+ashvardanian@users.noreply.github.com>
2022-12-27 18:17:03 +0400

    Make: Concurrent Wheel builds, Multi-Arch Docker

--- publish.sh
+++ publish.sh
@@ -5,7 +5,7 @@ version=$(cat VERSION)
 echo -e "------ \e[93mClean up\e[0m ------"
 rm -rf CMakeCache.txt CMakeFiles wheelhouse Makefile bin tmp/*
 
-# Build and test
+# Build and Test
 echo -e "------ \e[93mBuild\e[0m ------"
 cmake -DCMAKE_BUILD_TYPE=Release -DUKV_BUILD_TESTS=1 -B ./build_release \
       -DUKV_BUILD_ENGINE_UMEM=1 -DUKV_BUILD_ENGINE_LEVELDB=1 -DUKV_BUILD_ENGINE_ROCKSDB=1 -DUKV_BUILD_API_FLIGHT=0 \
@@ -21,7 +21,10 @@ echo -e "------ \e[92mTests Passing!\e[0m ------"
 
 # Build and Test Python
 echo -e "------ \e[93mBuild and Test Python\e[0m ------"
-cibuildwheel --platform linux
+CIBW_BUILD="cp37-*" cibuildwheel --platform linux &
+CIBW_BUILD="cp38-*" cibuildwheel --platform linux &
+CIBW_BUILD="cp39-*" cibuildwheel --platform linux &
+CIBW_BUILD="cp310-*" cibuildwheel --platform linux
 echo -e "------ \e[92mPython Tests Passing!\e[0m ------"
 
 # Publish Python
@@ -61,7 +64,7 @@ fi
 
 # Build Docker
 echo -e "\n------ \e[93mBuilding Docker\e[0m ------"
-docker build . --file docker/Dockerfile --tag unum/ukv:$version-focal
+docker buildx build --platform=linux/amd64,linux/arm64 . --file docker/Dockerfile --tag unum/ukv:$version-focal
 echo -e "------ \e[92mDocker Built!\e[0m ------"
 
 read -p "Publish to DockerHub? " -n 1 -r
