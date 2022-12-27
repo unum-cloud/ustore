f6cbcc6fb5b5d8d1c2b0219dc95a0c6d670d5acb
Darvin Harutyunyan <12006323+DarvinHarutyunyan@users.noreply.github.com>
2022-12-27 14:04:55 +0400

    Refactor: Java DB tests...

--- java/test/DataBaseLevelDBTest.java
+++ java/test/DataBaseLevelDBTest.java
@@ -1,16 +1,30 @@
+import java.io.File;
+import java.util.Arrays;
 
-import com.unum.ukv.DataBaseLevelDB;
 import org.junit.Test;
 
-import java.util.Arrays;
+import com.unum.ukv.DataBaseLevelDB;
 
 public class DataBaseLevelDBTest {
     static {
         DataBaseLevelDB.init();
     }
+
+    public static void deleteDirectoryFiles(File directory) {
+        if (directory.isDirectory()) {
+            File[] contents = directory.listFiles();
+            for (File f : contents)
+                f.delete();
+        }
+    }
+
     @Test
     public void test() {
-        DataBaseLevelDB.Context ctx = new DataBaseLevelDB.Context("./tmp");
+        String path = "./tmp/";
+        deleteDirectoryFiles(new File(path));
+
+        DataBaseLevelDB.Context ctx = new DataBaseLevelDB.Context(path);
+
         ctx.put(42, "hey".getBytes());
         assert Arrays.equals(ctx.get(42), "hey".getBytes()) : "Received wrong value";
 
--- java/test/DataBaseRocksDBTest.java
+++ java/test/DataBaseRocksDBTest.java
@@ -1,16 +1,31 @@
 
-import com.unum.ukv.DataBaseRocksDB;
+import java.io.File;
+import java.util.Arrays;
+
 import org.junit.Test;
 
-import java.util.Arrays;
+import com.unum.ukv.DataBaseRocksDB;
 
 public class DataBaseRocksDBTest {
     static {
         DataBaseRocksDB.init();
     }
+
+    public static void deleteDirectoryFiles(File directory) {
+        if (directory.isDirectory()) {
+            File[] contents = directory.listFiles();
+            for (File f : contents)
+                f.delete();
+        }
+    }
+
     @Test
     public void test() {
-        DataBaseRocksDB.Context ctx = new DataBaseRocksDB.Context("./tmp");
+        String path = "./tmp/";
+        deleteDirectoryFiles(new File(path));
+
+        DataBaseRocksDB.Context ctx = new DataBaseRocksDB.Context(path);
+
         ctx.put(42, "hey".getBytes());
         assert Arrays.equals(ctx.get(42), "hey".getBytes()) : "Received wrong value";
 
