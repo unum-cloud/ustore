02ebeaf5ef0ed100de9290b80bc42b06eddc6aa6
Davit Vardanyan <78792753+davvard@users.noreply.github.com>
2022-12-29 12:46:05 +0400

    Fix: from_json and to_string

--- python/nlohmann.hpp
+++ python/nlohmann.hpp
@@ -14,11 +14,19 @@ namespace unum::ukv::pyb {
 namespace py = pybind11;
 using json_t = nlohmann::json;
 
+inline static constexpr char int_to_hex_k[16] = {
+    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+inline void char_to_hex(uint8_t const c, uint8_t* hex) noexcept {
+    hex[0] = int_to_hex_k[c >> 4];
+    hex[1] = int_to_hex_k[c & 0x0F];
+}
+
 PyObject* from_json(nlohmann::json const& js) {
     if (js.is_null())
         return Py_None;
     else if (js.is_boolean())
-        return PyBool_FromLong(js.get<long>());
+        return js.get<bool>() ? Py_True : Py_False;
     else if (js.is_number_integer())
         return PyLong_FromLong(js.get<nlohmann::json::number_integer_t>());
     else if (js.is_number_float())
@@ -28,9 +36,9 @@ PyObject* from_json(nlohmann::json const& js) {
         return PyUnicode_FromStringAndSize(str.c_str(), str.size());
     }
     else if (js.is_array()) {
-        PyObject* obj = PyTuple_New(js.size());
+        PyObject* obj = PyList_New(js.size());
         for (size_t i = 0; i < js.size(); i++)
-            PyTuple_SetItem(obj, i, from_json(js[i]));
+            PyList_SetItem(obj, i, from_json(js[i]));
         return obj;
     }
     else // Object
@@ -43,7 +51,11 @@ PyObject* from_json(nlohmann::json const& js) {
 }
 
 inline void to_string(PyObject* obj, std::string& output) {
-    if (PyLong_Check(obj))
+    if (obj == Py_None)
+        output += "null";
+    else if (PyBool_Check(obj))
+        output += obj == Py_False ? "false" : "true";
+    else if (PyLong_Check(obj))
         fmt::format_to(std::back_inserter(output), "{}", PyLong_AsLong(obj));
     else if (PyFloat_Check(obj))
         fmt::format_to(std::back_inserter(output), "{}", PyFloat_AsDouble(obj));
@@ -55,31 +67,83 @@ inline void to_string(PyObject* obj, std::string& output) {
     }
     else if (PyUnicode_Check(obj)) {
         Py_ssize_t size;
-        auto str = PyUnicode_AsUTF8AndSize(obj, &size);
+        const char* char_ptr = PyUnicode_AsUTF8AndSize(obj, &size);
         output.reserve(output.size() + size + 2);
         output += "\"";
-        output += str;
+        for (std::size_t i = 0; i != size; ++i) {
+            uint8_t c = char_ptr[i];
+            switch (c) {
+            case 34: output += "\\\""; break;
+            case 92: output += "\\\\"; break;
+            case 8: output += "\\b"; break;
+            case 9: output += "\\t"; break;
+            case 10: output += "\\n"; break;
+            case 12: output += "\\f"; break;
+            case 13: output += "\\r"; break;
+            case 0:
+            case 1:
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+            case 7:
+            case 11:
+            case 14:
+            case 15:
+            case 16:
+            case 17:
+            case 18:
+            case 19:
+            case 20:
+            case 21:
+            case 22:
+            case 23:
+            case 24:
+            case 25:
+            case 26:
+            case 27:
+            case 28:
+            case 29:
+            case 30:
+            case 31: {
+                output += "\\u00";
+                auto target_ptr = reinterpret_cast<uint8_t*>(output.data() + output.size());
+                char_to_hex(c, target_ptr);
+                break;
+            }
+            default: output += char_ptr[i];
+            }
+        }
         output += "\"";
     }
     else if (PySequence_Check(obj)) {
         output += "[";
-        for (Py_ssize_t i = 0; i < PySequence_Length(obj); i++) {
-            to_string(PySequence_GetItem(obj, i), output);
-            output += ",";
+        if (!PySequence_Length(obj))
+            output += ']';
+        else {
+            for (Py_ssize_t i = 0; i < PySequence_Length(obj); i++) {
+                to_string(PySequence_GetItem(obj, i), output);
+                output += ",";
+            }
+            output[output.size() - 1] = ']';
         }
-        output[output.size() - 1] = ']';
     }
     else if (PyDict_Check(obj)) {
         output += "{";
-        PyObject *key, *value;
-        Py_ssize_t pos = 0;
-        while (PyDict_Next(obj, &pos, &key, &value)) {
-            to_string(key, output);
-            output += ":";
-            to_string(value, output);
-            output += ",";
+        if (!PyDict_Size(obj))
+            output += "}";
+        else {
+            PyObject *key, *value;
+            Py_ssize_t pos = 0;
+            while (PyDict_Next(obj, &pos, &key, &value)) {
+                to_string(key, output);
+                output += ":";
+                to_string(value, output);
+                output += ",";
+            }
+            output[output.size() - 1] = '}';
         }
-        output[output.size() - 1] = '}';
     }
     else
         throw std::runtime_error("invalid type for conversion.");
