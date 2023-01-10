619deffa85bb368d7669af699e8bb7897da66a8a
Violeta Stepanyan <82702795+VioletaStepanyan@users.noreply.github.com>
2023-01-10 13:21:31 +0400

    Fix: leveldb ukv_snapshot_create.

--- src/engine_leveldb.cpp
+++ src/engine_leveldb.cpp
@@ -174,13 +174,13 @@ void ukv_snapshot_list(ukv_snapshot_list_t*) {
 
 void ukv_snapshot_create(ukv_snapshot_create_t* c_ptr) {
     ukv_snapshot_create_t& c = *c_ptr;
-    level_db_t& db = *reinterpret_cast<level_db_t*>(c.db);
-    level_snapshot_t& snp = **reinterpret_cast<level_snapshot_t**>(c.snapshot);
-
     return_error_if_m(c.db, c.error, uninitialized_state_k, "DataBase is uninitialized");
     if (!*c.snapshot)
         safe_section("Allocating snapshot handle", c.error, [&] { *c.snapshot = new level_snapshot_t(); });
     return_if_error_m(c.error);
+
+    level_db_t& db = *reinterpret_cast<level_db_t*>(c.db);
+    level_snapshot_t& snp = **reinterpret_cast<level_snapshot_t**>(c.snapshot);
 
     snp.snapshot = db.GetSnapshot();
     if (!*c.snapshot)
