54fa5dcce34dc8acc9bade2e612fb3480f4d1995
Davit Vardanyan <78792753+davvard@users.noreply.github.com>
2023-01-11 18:53:29 +0400

    Perf: Compute partition without reading neighbors from db

--- python/community/louvain.py
+++ python/community/louvain.py
@@ -1,55 +1,56 @@
-def get_degree_of_node_in_community(graph,node, partition,community):
-    result=0
-    for neighbor in graph.neighbors(node):
-        if partition[neighbor] == community:
-            result+=1
-    return result
+import random
 
-def best_partition(graph):
+
+def best_partition(graph, max_community_size=100, min_modularity_growth=0.0000001):
+
+    if (max_community_size > graph.number_of_nodes()):
+        max_community_size = graph.number_of_nodes()
+
     partition = {}
     degrees = {}
-    com_in_degree = {}
     com_tot_degree = {}
+    node_com_degrees = {}
+    degreview = graph.degree
 
-    for node in graph.nodes:
-        partition[node] = node
-        degrees[node] = graph.degree(node)
-        com_in_degree[node] = 0
-     
-    com_tot_degree=degrees.copy()
-    m2 = graph.number_of_edges() * 2
+    for node in graph.nodes():
+        degrees[node] = degreview[node]
+        partition[node] = random.randint(0, max_community_size-1)
+        node_com_degrees[node] = [0] * max_community_size
+    com_tot_degree = degrees.copy()
 
-    for node,neighbor in graph.edges:
-        if partition[node] == partition[neighbor]:
-            continue
-        community = partition[node]
-        new_community = partition[neighbor]
+    modified = True
+    while modified:
+        for node in node_com_degrees.keys():
+            node_com_degrees[node] = [0] * max_community_size
 
-        K_i = degrees[node]
-        E_in = com_in_degree[new_community]
-        E_tot = com_tot_degree[new_community]
-        K_i_in = get_degree_of_node_in_community(graph,node,partition,new_community)
-        Q_After = (E_in + K_i_in)/m2 - ((E_tot + K_i)/m2 ** 2)
-        Q_Before = (E_in/m2) - ((E_tot/m2)**2) - ((K_i/m2) ** 2)
-        delta_merge = Q_After - Q_Before
+        links = 0
+        for node, neighbor in graph.edges():
+            links += 1
+            node_com_degrees[node][partition[neighbor]] += 1
+            node_com_degrees[neighbor][partition[node]] += 1
 
-        E_in = com_in_degree[community]
-        E_tot = com_tot_degree[community]
-        K_i_in = get_degree_of_node_in_community(graph,node,partition,community)
-        Q_After = (E_in + K_i_in)/m2 - ((E_tot + K_i)/m2) ** 2
-        Q_Before = (E_in/m2) - (E_tot/m2)**2
-        delta_remove = Q_After - Q_Before
+        modified = False
+        for node, neighbor in graph.edges():
+            if partition[node] == partition[neighbor]:
+                continue
 
-        delta = delta_remove + delta_merge
-        if delta>0:
-            deg = degrees[node] 
-            com_tot_degree[community] -= deg
-            com_tot_degree[new_community] += deg
-            for n in graph.neighbors(node):
-                if partition[n] == community:
-                    com_in_degree[community]-=1
-                if partition[n] == new_community:   
-                    com_in_degree[new_community]+=1
-            partition[node] = partition[neighbor]
+            community_node = partition[node]
+            community_neighbor = partition[neighbor]
+            deg = degrees[node]
+            node_com_tot_degree = com_tot_degree[community_node]
+            neighbor_com_tot_degree = com_tot_degree[community_neighbor]
+            degree_in_node_com = node_com_degrees[node][community_node]
+            degree_in_neighbor_com = node_com_degrees[node][community_neighbor]
+
+            delta_modularity = (1/links) * (degree_in_neighbor_com-degree_in_node_com) - \
+                (deg/(2*(links**2))) * \
+                (deg + neighbor_com_tot_degree - node_com_tot_degree)
+
+            if delta_modularity > min_modularity_growth:
+                com_tot_degree[community_node] -= (deg - degree_in_node_com)
+                com_tot_degree[community_neighbor] += (
+                    deg - degree_in_neighbor_com)
+                partition[node] = partition[neighbor]
+                modified = True
+
     return partition
-
