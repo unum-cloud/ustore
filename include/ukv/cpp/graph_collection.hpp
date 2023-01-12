341b4022ebb8276cb9ce8be54db1dfe185d3c8ed
Davit Vardanyan <78792753+davvard@users.noreply.github.com>
2023-01-12 18:38:39 +0400

    Refactor: graph neighbors

--- include/ukv/cpp/graph_collection.hpp
+++ include/ukv/cpp/graph_collection.hpp
@@ -373,11 +373,12 @@ class graph_collection_t {
         return strided_range_gt<ukv_key_t> {maybe->source_ids};
     }
 
-    expected_gt<strided_range_gt<ukv_key_t>> neighbors(ukv_key_t vertex) noexcept {
+    expected_gt<strided_range_gt<ukv_key_t>> neighbors(ukv_key_t vertex,
+                                                       ukv_vertex_role_t role = ukv_vertex_role_any_k) noexcept {
         // Retrieving neighbors in directed graphs is trickier than just `successors` or `predecessors`.
         // We are receiving an adjacency list, where both incoming an edges exist.
         // So the stride/offset is not uniform across the entire list.
-        auto maybe = edges(vertex, ukv_vertex_role_any_k);
+        auto maybe = edges(vertex, role);
         if (!maybe)
             return maybe.release_status();
 
