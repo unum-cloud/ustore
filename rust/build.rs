248df056a9646b2b38577061f1b6f3cbd8f713d9
michaelgrigoryan25 <56165400+michaelgrigoryan25@users.noreply.github.com>
2023-01-27 10:30:26 +0400

    .

--- rust/build.rs
+++ rust/build.rs
@@ -4,10 +4,8 @@ use std::env::var;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-	// Tell cargo to invalidate the built crate whenever the wrapper changes
 	println!("cargo:rerun-if-changed=wrapper.h");
-	// Tell cargo to look for shared libraries in the specified directory
-	println!("cargo:rustc-link-search=../include/ukv");
+	// println!("cargo:rustc-link-search=../include");
 
 	let bindings = bindgen::Builder::default()
 		.header("./wrapper.h")
@@ -16,7 +14,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		.parse_callbacks(Box::new(bindgen::CargoCallbacks))
 		.generate()?;
 
-	// Write the bindings to the $OUT_DIR/bindings.rs file.
 	let out_path = PathBuf::from(var("OUT_DIR")?);
 	bindings.write_to_file(out_path.join("bindings.rs"))?;
 	Ok(())
