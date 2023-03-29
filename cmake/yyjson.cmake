fded0dcaba80ed18e47cacc8b55ec7aeaa985a38
Darvin Harutyunyan <12006323+DarvinHarutyunyan@users.noreply.github.com>
2023-03-29 16:36:46 +0400

    Upgrade: yyjson version

--- cmake/yyjson.cmake
+++ cmake/yyjson.cmake
@@ -2,7 +2,7 @@ include(FetchContent)
 FetchContent_Declare(
     yyjson
     GIT_REPOSITORY https://github.com/ibireme/yyjson
-    GIT_TAG 0.5.1
+    GIT_TAG 0.6.0
 )
 FetchContent_MakeAvailable(yyjson)
 set_property(TARGET yyjson PROPERTY POSITION_INDEPENDENT_CODE ON)
