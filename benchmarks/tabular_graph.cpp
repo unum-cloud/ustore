490134ea0b8bd7bf3e246607c7adc408729eb98a
Arman Ghazaryan <91345263+Arman-Ghazaryan@users.noreply.github.com>
2023-04-22 15:53:00 +0000

    Add: bench. for tools(graph interaface)

--- benchmarks/tabular_graph.cpp
+++ benchmarks/tabular_graph.cpp
@@ -48,6 +48,9 @@ struct args_t {
 
     std::string config_path;
 
+    std::string source;
+    std::string target;
+    std::string edge;
     std::string id;
 
     size_t threads_count;
@@ -60,6 +63,9 @@ void parse_args(int argc, char* argv[], args_t& args) {
     program.add_argument("-e", "--ext").required().help("File extension for exporting");
     program.add_argument("-c", "--cfg").default_value(std::string("")).help("Config path");
     program.add_argument("-i", "--id").required().help("Id field");
+    program.add_argument("-s", "--source").required().help("Source field");
+    program.add_argument("-t", "--target").required().help("Target field");
+    program.add_argument("-ed", "--edge").required().help("Edge field");
     program.add_argument("-th", "--threads").default_value(std::string("1")).help("Threads count");
     program.add_argument("-m", "--max_input_files").default_value(std::string("10")).help("Max input files count");
 
@@ -68,6 +74,9 @@ void parse_args(int argc, char* argv[], args_t& args) {
     args.path = program.get("path");
     args.extension = program.get("ext");
     args.config_path = program.get("cfg");
+    args.source = program.get("source");
+    args.target = program.get("target");
+    args.edge = program.get("edge");
     args.id = program.get("id");
     args.threads_count = std::stoi(program.get("threads"));
     args.files_count = std::stoi(program.get("max_input_files"));
@@ -82,6 +91,34 @@ void parse_args(int argc, char* argv[], args_t& args) {
         exit(1);
     }
 }
+
+size_t get_keys_count() {
+    static std::mutex mtx;
+    mtx.lock();
+    status_t status;
+    arena_t arena(db);
+    ustore_collection_t collection = db.main();
+
+    ustore_key_t key = 0;
+    ustore_length_t counts = -1;
+    ustore_length_t* found_counts = nullptr;
+    ustore_key_t* keys = nullptr;
+
+    ustore_scan_t scan {};
+    scan.db = db;
+    scan.error = status.member_ptr();
+    scan.arena = arena.member_ptr();
+    scan.tasks_count = 1;
+    scan.collections = &collection;
+    scan.start_keys = &key;
+    scan.count_limits = &counts;
+    scan.counts = &found_counts;
+    scan.keys = &keys;
+    ustore_scan(&scan);
+    mtx.unlock();
+    return (*found_counts * 3);
+}
+
 
 static void bench_docs_import(bm::State& state, args_t const& args) {
     auto collection = db.main();
@@ -111,6 +148,38 @@ static void bench_docs_import(bm::State& state, args_t const& args) {
         ++idx;
     }
     auto end = std::chrono::high_resolution_clock::now();
+    double duration = std::chrono::duration<double, std::milli>(end - start).count() / 1000;
+    state.counters["bytes/s"] = bm::Counter(size / duration);
+    state.counters["duration"] = bm::Counter(duration, bm::Counter::kAvgThreads);
+    state.counters["imported"] = bm::Counter(size);
+}
+
+static void bench_graph_import(bm::State& state, args_t const& args) {
+    auto collection = db.main();
+    arena_t arena(db);
+    status_t status;
+    size_t idx = 0;
+
+    auto start = std::chrono::high_resolution_clock::now();
+    for (auto _ : state) {
+        ustore_graph_import_t graph {};
+        graph.db = db;
+        graph.error = status.member_ptr();
+        graph.arena = arena.member_ptr();
+        graph.collection = collection;
+        graph.paths_pattern = source_files[state.thread_index() + idx].c_str();
+        graph.max_batch_size = max_batch_size_k;
+        graph.source_id_field = args.source.c_str();
+        graph.target_id_field = args.target.c_str();
+        graph.edge_id_field = args.edge.c_str();
+        ustore_graph_import(&graph);
+
+        if (!status)
+            status.release_error();
+        ++idx;
+    }
+    auto end = std::chrono::high_resolution_clock::now();
+    size_t size = get_keys_count() * sizeof(ustore_key_t);
     double duration = std::chrono::duration<double, std::milli>(end - start).count() / 1000;
     state.counters["bytes/s"] = bm::Counter(size / duration);
     state.counters["duration"] = bm::Counter(duration, bm::Counter::kAvgThreads);
@@ -169,6 +238,47 @@ static void bench_docs_export(bm::State& state, args_t const& args) {
     db.clear().throw_unhandled();
 }
 
+static void bench_graph_export(bm::State& state, args_t const& args) {
+    auto collection = db.main();
+    arena_t arena(db);
+    status_t status;
+
+    size_t size = 0;
+
+    std::string source = args.source;
+    std::string target = args.target;
+    std::string edge = args.edge;
+
+    std::replace(source.begin(), source.end(), '/', '_');
+    std::replace(target.begin(), target.end(), '/', '_');
+    std::replace(edge.begin(), edge.end(), '/', '_');
+
+    for (auto _ : state) {
+        ustore_graph_export_t graph {};
+        graph.db = db;
+        graph.error = status.member_ptr();
+        graph.arena = arena.member_ptr();
+        graph.collection = collection;
+        graph.paths_extension = args.extension.c_str();
+        graph.max_batch_size = max_batch_size_k;
+        graph.source_id_field = source.c_str();
+        graph.target_id_field = target.c_str();
+        graph.edge_id_field = edge.c_str();
+        ustore_graph_export(&graph);
+
+        if (status)
+            size += find_and_delete();
+        else
+            status.release_error();
+    }
+    db.clear().throw_unhandled();
+    auto end = std::chrono::high_resolution_clock::now();
+    double duration = std::chrono::duration<double, std::milli>(end - start).count() / 1000;
+    state.counters["bytes/s"] = bm::Counter(size / duration);
+    state.counters["duration"] = bm::Counter(duration);
+    state.counters["exported"] = bm::Counter(size);
+}
+
 void parse_paths(args_t& args) {
     fmt::print("Will search for {} files...\n", args.extension);
     auto dataset_path = args.path;
@@ -198,6 +308,16 @@ void bench_docs(args_t const& args) {
                           [&](bm::State& s) { bench_docs_export(s, args); });
 }
 
+void bench_graph(args_t const& args) {
+    bm::RegisterBenchmark(fmt::format("graph_import_{}", args.extension.substr(1)).c_str(),
+                          [&](bm::State& s) { bench_graph_import(s, args); })
+        ->Threads(args.threads_count)
+        ->Iterations(args.files_count);
+    bm::RegisterBenchmark(fmt::format("graph_export_{}", args.extension.substr(1)).c_str(), [&](bm::State& s) {
+        bench_graph_export(s, args);
+    })->Iterations(1);
+}
+
 int main(int argc, char** argv) {
 
     args_t args {};
@@ -207,6 +327,7 @@ int main(int argc, char** argv) {
     db.open(args.config_path.c_str()).throw_unhandled();
 
     bench_docs(args);
+    bench_graph(args);
 
     bm::RunSpecifiedBenchmarks();
     bm::Shutdown();
