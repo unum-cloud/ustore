c7543e1c39be82d3a94785941557d54cb59ca73e
Violeta Stepanyan <82702795+VioletaStepanyan@users.noreply.github.com>
2023-04-27 14:53:38 +0400

    Refactor: snapshot export interface.

--- include/ustore/db.h
+++ include/ustore/db.h
@@ -271,21 +271,6 @@ void ustore_database_init(ustore_database_init_t*);
 /***************** Snapshot Management  ****************/
 /*********************************************************/
 
-
-typedef struct ustore_snapshot_export_t {
-    /** @brief Already open database instance. */
-    ustore_database_t db;
-    /** @brief Pointer to exported error message. */
-    ustore_error_t* error;
-    /** @brief Snapshot id. */
-    ustore_snapshot_t* id;
-     /** @brief The specified directory path. */ 
-     ustore_char_t* path;
-    /// @}
-} ustore_snapshot_export_t;
-
-void ustore_snapshot_export(ustore_snapshot_export_t*);
-
 typedef struct ustore_snapshot_list_t { /// @name Context
     /// @{
 
@@ -336,6 +321,20 @@ typedef struct ustore_snapshot_create_t {
 } ustore_snapshot_create_t;
 
 void ustore_snapshot_create(ustore_snapshot_create_t*);
+
+typedef struct ustore_snapshot_export_t {
+    /** @brief Already open database instance. */
+    ustore_database_t db;
+    /** @brief Pointer to exported error message. */
+    ustore_error_t* error;
+    /** @brief Snapshot id. */
+    ustore_snapshot_t id;
+    /** @brief The specified directory path. */
+    ustore_str_view_t path;
+    /// @}
+} ustore_snapshot_export_t;
+
+void ustore_snapshot_export(ustore_snapshot_export_t*);
 
 typedef struct ustore_snapshot_drop_t {
     /** @brief Already open database instance. */
