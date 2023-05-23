709de76cf477d753a853707e21af829a014bc336
Davit Vardanyan <78792753+davvard@users.noreply.github.com>
2023-05-23 13:36:06 +0400

    Add: Redis transaction

--- src/redis_client.cpp
+++ src/redis_client.cpp
@@ -15,7 +15,7 @@
 ustore_collection_t const ustore_collection_main_k = 0;
 ustore_length_t const ustore_length_missing_k = std::numeric_limits<ustore_length_t>::max();
 ustore_key_t const ustore_key_unknown_k = std::numeric_limits<ustore_key_t>::max();
-bool const ustore_supports_transactions_k = false;
+bool const ustore_supports_transactions_k = true;
 bool const ustore_supports_named_collections_k = true;
 bool const ustore_supports_snapshots_k = false;
 static const char kDefaultCollectionName[] = "default";
@@ -26,24 +26,49 @@ static const char kDefaultCollectionName[] = "default";
 
 using namespace unum::ustore;
 using namespace unum;
-using namespace sw::redis;
 
-struct redis_client_t {
-    std::unique_ptr<Redis> native;
-    std::vector<std::string> collections;
-};
-
-inline StringView to_string_view(byte_t const* p, size_t size_bytes) noexcept {
+inline sw::redis::StringView to_string_view(byte_t const* p, size_t size_bytes) noexcept {
     return {reinterpret_cast<const char*>(p), size_bytes};
 }
 
-inline StringView to_string_view(ustore_key_t const& k) noexcept {
+inline sw::redis::StringView to_string_view(ustore_key_t const& k) noexcept {
     return {reinterpret_cast<const char*>(&k), sizeof(ustore_key_t)};
 }
 
-inline StringView redis_collection(ustore_collection_t collection) {
+inline sw::redis::StringView redis_collection(ustore_collection_t collection) {
     return collection == ustore_collection_main_k ? kDefaultCollectionName : reinterpret_cast<const char*>(collection);
 }
+
+struct redis_txn_t {
+    std::unique_ptr<sw::redis::Transaction> native;
+    std::map<collection_key_t, sw::redis::OptionalString> uncommited;
+
+    sw::redis::OptionalString get_uncommited(ustore_collection_t collection, ustore_key_t key) {
+        auto it = uncommited.find({collection, key});
+        if (it != uncommited.end())
+            return it->second;
+    }
+
+    void set(ustore_collection_t collection, ustore_key_t key, value_view_t value) {
+        native->hset(redis_collection(collection), to_string_view(key), to_string_view(value.data(), value.size()));
+        uncommited[{collection, key}] = std::string((const char*)value.data(), value.size());
+    }
+
+    void del(ustore_collection_t collection, ustore_key_t key) {
+        native->hdel(redis_collection(collection), to_string_view(key));
+        uncommited.erase({collection, key});
+    }
+
+    void exec() {
+        native->exec();
+        uncommited.clear();
+    }
+};
+
+struct redis_client_t {
+    std::unique_ptr<sw::redis::Redis> native;
+    std::vector<std::string> collections;
+};
 
 /*********************************************************/
 /*****************	    C Interface 	  ****************/
@@ -53,12 +78,13 @@ void ustore_database_init(ustore_database_init_t* c_ptr) {
     ustore_database_init_t& c = *c_ptr;
 
     safe_section("Starting client", c.error, [&] {
-        ConnectionOptions connection_options;
+        sw::redis::ConnectionOptions connection_options;
         connection_options.host = "127.0.0.1";
         connection_options.port = 6379;
 
         redis_client_t* db_ptr = new redis_client_t;
-        db_ptr->native = std::make_unique<Redis>(connection_options);
+        db_ptr->native = std::make_unique<sw::redis::Redis>(connection_options);
+
         db_ptr->native->keys("*", std::back_inserter(db_ptr->collections));
         auto it = std::find(db_ptr->collections.begin(), db_ptr->collections.end(), kDefaultCollectionName);
         if (it != db_ptr->collections.end())
@@ -72,6 +98,7 @@ void ustore_read(ustore_read_t* c_ptr) {
     ustore_read_t& c = *c_ptr;
     return_error_if_m(c.db, c.error, uninitialized_state_k, "DataBase is uninitialized");
     redis_client_t& db = *reinterpret_cast<redis_client_t*>(c.db);
+    redis_txn_t& txn = *reinterpret_cast<redis_txn_t*>(c.transaction);
 
     linked_memory_lock_t arena = linked_memory(c.arena, c.options, c.error);
     return_if_error_m(c.error);
@@ -95,15 +122,27 @@ void ustore_read(ustore_read_t* c_ptr) {
         uninitialized_array_gt<byte_t> contents(arena);
         for (std::size_t i = 0; i != places.size(); ++i) {
             place_t place = places[i];
-            auto value = db.native->hget(redis_collection(place.collection), to_string_view(place.key));
+            sw::redis::OptionalString value;
+
+            if (c.transaction) {
+                value = txn.get_uncommited(place.collection, place.key);
+                if (!value)
+                    value = db.native->hget(redis_collection(place.collection), to_string_view(place.key));
+                txn.native->hget(redis_collection(place.collection), to_string_view(place.key));
+            }
+            else
+                value = db.native->hget(redis_collection(place.collection), to_string_view(place.key));
+
+            offs[i] = contents.size();
             presences[i] = bool(value);
             lens[i] = value ? value->size() : ustore_length_missing_k;
-            offs[i] = contents.size();
-            if (needs_export && value)
+            if (needs_export && value) {
                 contents.insert(contents.size(),
                                 (byte_t*)value->data(),
                                 (byte_t*)(value->data() + value->size()),
                                 c.error);
+                return_if_error_m(c.error);
+            }
         }
         offs[places.count] = contents.size();
         if (needs_export)
@@ -116,6 +155,7 @@ void ustore_write(ustore_write_t* c_ptr) {
     return_error_if_m(c.db, c.error, uninitialized_state_k, "DataBase is uninitialized");
 
     redis_client_t& db = *reinterpret_cast<redis_client_t*>(c.db);
+    redis_txn_t& txn = *reinterpret_cast<redis_txn_t*>(c.transaction);
     strided_iterator_gt<ustore_key_t const> keys {c.keys, c.keys_stride};
     strided_iterator_gt<ustore_collection_t const> collections {c.collections, c.collections_stride};
     strided_iterator_gt<ustore_bytes_cptr_t const> vals {c.values, c.values_stride};
@@ -133,11 +173,20 @@ void ustore_write(ustore_write_t* c_ptr) {
         for (std::size_t i = 0; i != places.size(); ++i) {
             auto place = places[i];
             auto content = contents[i];
-            auto collection = redis_collection(place.collection);
-            if (content)
-                db.native->hset(collection, to_string_view(place.key), to_string_view(content.data(), content.size()));
-            else
-                db.native->hdel(collection, to_string_view(place.key));
+            if (content) {
+                if (c.transaction)
+                    txn.set(place.collection, place.key, content);
+                else
+                    db.native->hset(redis_collection(place.collection),
+                                    to_string_view(place.key),
+                                    to_string_view(content.data(), content.size()));
+            }
+            else {
+                if (c.transaction)
+                    txn.del(place.collection, place.key);
+                else
+                    db.native->hdel(redis_collection(place.collection), to_string_view(place.key));
+            }
         }
     });
 }
@@ -308,9 +357,31 @@ void ustore_snapshot_export(ustore_snapshot_export_t* c_ptr) {
 /*********************************************************/
 
 void ustore_transaction_init(ustore_transaction_init_t* c_ptr) {
+    ustore_transaction_init_t& c = *c_ptr;
+    return_error_if_m(c.db, c.error, uninitialized_state_k, "DataBase is uninitialized");
+    validate_transaction_begin(c.transaction, c.options, c.error);
+    return_if_error_m(c.error);
+
+    redis_client_t& db = *reinterpret_cast<redis_client_t*>(c.db);
+    safe_section("Initializing Transaction", c.error, [&] {
+        auto txn_ptr = new redis_txn_t;
+        txn_ptr->native = std::make_unique<sw::redis::Transaction>(db.native->transaction());
+        *c.transaction = txn_ptr;
+    });
 }
 
 void ustore_transaction_commit(ustore_transaction_commit_t* c_ptr) {
+    ustore_transaction_commit_t& c = *c_ptr;
+    if (!c.transaction)
+        return;
+
+    validate_transaction_commit(c.transaction, c.options, c.error);
+    return_if_error_m(c.error);
+
+    redis_client_t& db = *reinterpret_cast<redis_client_t*>(c.db);
+    redis_txn_t& txn = *reinterpret_cast<redis_txn_t*>(c.transaction);
+    safe_section("Commiting Transaction", c.error, [&] { txn.exec(); });
+    return_if_error_m(c.error);
 }
 
 /*********************************************************/
@@ -322,6 +393,9 @@ void ustore_arena_free(ustore_arena_t c_arena) {
 }
 
 void ustore_transaction_free(ustore_transaction_t const c_transaction) {
+    if (!c_transaction)
+        return;
+    delete reinterpret_cast<redis_txn_t*>(c_transaction);
 }
 
 void ustore_database_free(ustore_database_t c_db) {
