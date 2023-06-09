3f0178ec5234286763651d377e8f6661f181b4e5
violeta <violeta@localhost>
2023-06-09 12:22:00 +0400

    Add:  benchmark for ustore sample.

--- benchmarks/twitter.cpp
+++ benchmarks/twitter.cpp
@@ -380,6 +380,60 @@ void sample_tweet_id_batches(bm::State& state, callback_at callback) {
     state.counters["fails,%"] = bm::Counter((iterations - successes) * 100.0, bm::Counter::kAvgThreads);
 }
 
+template <typename callback_at>
+void native_sample(bm::State& state, callback_at callback) {
+
+    auto const batch_size = static_cast<ustore_size_t>(state.range(0));
+    std::vector<ustore_key_t> batch_keys(batch_size);
+
+    std::size_t iterations = 0;
+    std::size_t successes = 0;
+    for (auto _ : state) {
+        successes += callback(batch_size);
+        iterations++;
+    }
+
+    state.counters["items/s"] = bm::Counter(iterations * batch_size, bm::Counter::kIsRate);
+    state.counters["batches/s"] = bm::Counter(iterations, bm::Counter::kIsRate);
+    state.counters["fails,%"] = bm::Counter((iterations - successes) * 100.0, bm::Counter::kAvgThreads);
+}
+
+static void docs_sample_keys(bm::State& state) {
+
+    arena_t arena(db);
+
+    std::size_t received_bytes = 0;
+    native_sample(state, [&](ustore_size_t count) {
+        ustore_length_t limit_count = 1;
+        ustore_length_t* found_offsets = nullptr;
+        ustore_length_t* found_counts = nullptr;
+        ustore_key_t* found_keys = nullptr;
+
+        status_t status;
+        ustore_sample_t sample {};
+        sample.db = db;
+        sample.error = status.member_ptr();
+        sample.arena = arena.member_ptr();
+        sample.tasks_count = count;
+        sample.collections = &collection_docs_k;
+        sample.count_limits = &limit_count;
+        sample.count_limits_stride = 0;
+        sample.offsets = &found_offsets;
+        sample.counts = &found_counts;
+        sample.keys = &found_keys;
+
+        ustore_sample(&sample);
+        if (!status)
+            return false;
+
+        received_bytes += found_offsets[count];
+        return true;
+    });
+
+    state.counters["bytes/s"] = bm::Counter(received_bytes, bm::Counter::kIsRate);
+    state.counters["bytes/it"] = bm::Counter(received_bytes, bm::Counter::kAvgIterations);
+}
+
 static void docs_sample_blobs(bm::State& state) {
 
     arena_t arena(db);
@@ -797,6 +851,14 @@ int main(int argc, char** argv) {
             ->Arg(settings.small_batch_size)
             ->Arg(settings.mid_batch_size)
             ->Arg(settings.big_batch_size);
+
+    bm::RegisterBenchmark("docs_sample_keys", &docs_sample_keys) //
+        ->MinTime(settings.min_seconds)
+        ->UseRealTime()
+        ->Threads(settings.threads_count)
+        ->Arg(settings.small_batch_size)
+        ->Arg(settings.mid_batch_size)
+        ->Arg(settings.big_batch_size);
 
     bm::RegisterBenchmark("docs_sample_objects", &docs_sample_objects) //
         ->MinTime(settings.min_seconds)
