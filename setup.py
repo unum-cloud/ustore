a38862096321e7e42565c48fe919b4b84b0418e6
Darvin Harutyunyan <12006323+DarvinHarutyunyan@users.noreply.github.com>
2023-08-03 19:29:35 +0400

    Build: Ustore python wheels by 'USE_CONAN' flag

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ class CMakeBuild(build_ext):
             '-DUSTORE_BUILD_SDK_PYTHON=1',
             '-DUSTORE_BUILD_TESTS=0',
             '-DUSTORE_USE_PYARROW=1',
-            '-DUSE_CONAN=0'
+            '-DUSE_CONAN=1'
         ]
 
         # Adding CMake arguments set as environment variable
